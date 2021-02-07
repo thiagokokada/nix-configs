62fdbfdfa9a464648ef0a57423d86aa78adbe397
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-02-07 19:02:33 -0300

    nixos: add nixos/theme.nix
    Port of /etc/nixos/theme-configuration.nix.

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
           ./nixos/dev.nix
           ./nixos/game.nix
           ./nixos/misc.nix
+          ./nixos/theme.nix
           ./nixos/xserver.nix
           ./overlays
         ];
@@ -34,6 +35,7 @@
           ./nixos/laptop.nix
           ./nixos/misc.nix
           # ./nixos/optimus.nix
+          ./nixos/theme.nix
           ./nixos/xserver.nix
           ./overlays
         ];
