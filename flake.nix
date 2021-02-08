006859a0a913f9fc2f67543a23d7a3b2dd58197b
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-02-08 20:26:11 -0300

    flake: re-enable home build using workaround

--- flake.nix
+++ flake.nix
@@ -52,7 +52,10 @@
 
     # https://github.com/nix-community/home-manager/issues/1510
     homeConfigurations.home = home.lib.homeManagerConfiguration rec {
-      configuration = ./home-manager/home.nix;
+      configuration = import ./home-manager/home.nix { } // {
+        _module.args.inputs = inputs;
+        _module.args.system = system;
+      };
       system = "x86_64-linux";
       homeDirectory = "/home/thiagoko";
       username = "thiagoko";
@@ -60,8 +63,6 @@
       # extraSpecialArgs = { inherit inputs system; };
     };
 
-    # home = self.homeConfigurations.home.activationPackage;
-    home = throw
-      "Disabled until https://github.com/nix-community/home-manager/pull/1790 is merged";
+    home = self.homeConfigurations.home.activationPackage;
   };
 }
