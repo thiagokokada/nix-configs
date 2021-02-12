11ffc3453121988b3973f8ba465c07adcc3e9e6b
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-02-11 22:40:11 -0300

    home-manager/misc: allowUnfree = true, add hm alias

--- home-manager/misc.nix
+++ home-manager/misc.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
+  nixpkgs.config = import ./nixpkgs-config.nix;
+  xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
+
   imports = [ ../overlays ];
+
+  programs.zsh.shellAliases = {
+    "hm" = "home-manager";
+  };
 }
