dfcfc05ee15618a37a2b54787d532e2d4b88a2f4
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-02-12 22:42:44 -0300

    nixos: move libvirt config to its own file, add VM scripts

---
+++ nixos/libvirt.nix
@@ -0,0 +1,109 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (config.my) username;
+  reservedGuestCpus = "2-5";
+  startVmScript = name:
+    pkgs.writeShellScriptBin "start-${name}" ''
+      sudo -s -- <<EOF
+      # Make sure we have sufficient memory for hugepages
+      sync
+      echo 3 | tee /proc/sys/vm/drop_caches
+      echo 1 | tee /proc/sys/vm/compact_memory
+
+      # Reduce VM jitter: https://www.kernel.org/doc/Documentation/kernel-per-CPU-kthreads.txt
+      ${pkgs.procps}/bin/sysctl vm.stat_interval=120
+      # the kernel's dirty page writeback mechanism uses kthread workers. They introduce
+      # massive arbitrary latencies when doing disk writes on the host and aren't
+      # migrated by cset. Restrict the workqueue to use only cpu 0.
+      echo 00 | tee /sys/bus/workqueue/devices/writeback/cpumask
+
+      ${pkgs.cpusetWithPatch}/bin/cset shield --reset
+      ${pkgs.cpusetWithPatch}/bin/cset shield --cpu "${reservedGuestCpus}" --kthread=on
+      ${pkgs.libvirt}/bin/virsh start "${name}"
+      EOF
+    '';
+
+  stopVmScript = name:
+    pkgs.writeShellScriptBin "stop-${name}" ''
+      sudo -s -- <<EOF
+      ${pkgs.libvirt}/bin/virsh shutdown "${name}"
+      # All VMs offline
+      echo ff | tee /sys/bus/workqueue/devices/writeback/cpumask
+      ${pkgs.procps}/bin/sysctl vm.stat_interval=1
+      ${pkgs.cpusetWithPatch}/bin/cset shield --reset
+      EOF
+    '';
+
+in {
+  boot = {
+    # Early load i195 for better resolution in init.
+    initrd.kernelModules = [ "i915" ];
+
+    # Do not load NVIDIA drivers.
+    blacklistedKernelModules = [ "nvidia" "nouveau" "hid-uclogic" ];
+
+    # Load VFIO related modules.
+    kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+    extraModprobeConfig = "options vfio-pci ids=10de:1c02,10de:10f1";
+
+    # Enable IOMMU
+    kernelParams = [ "intel_iommu=on" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    (startVmScript "win10")
+    (stopVmScript "win10")
+    cpusetWithPatch
+    virtmanager
+  ];
+
+  networking = {
+    # Needs to disable global DHCP to use bridge interfaces
+    useDHCP = false;
+    interfaces.br0.useDHCP = true;
+
+    # Enable bridge
+    bridges = {
+      br0 = {
+        interfaces = if config.networking.usePredictableInterfaceNames then
+          [ "eno1" ]
+        else
+          [ "eth0" ];
+      };
+    };
+  };
+
+  # Enable libvirtd
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemuOvmf = true;
+      onBoot = "ignore";
+      onShutdown = "shutdown";
+      qemuVerbatimConfig = ''
+        nographics_allow_host_audio = 1
+      '';
+    };
+  };
+
+  # Add user to libvirtd group.
+  users.users.${username} = { extraGroups = [ "libvirtd" ]; };
+
+  systemd.services.setup-win10-vm = {
+    after = [ "libvirtd.service" ];
+    requires = [ "libvirtd.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+    };
+    script = ''
+      uuid="$(${pkgs.libvirt}/bin/virsh domuuid win10 || true)"
+      ${pkgs.libvirt}/bin/virsh define <(sed "s/@UUID@/$uuid/" ${./win10.xml})
+    '';
+  };
+
+  # Reduce latency.
+  powerManagement.cpuFreqGovernor = "performance";
+}
--- nixos/pc.nix
+++ nixos/pc.nix
@@ -4,8 +4,10 @@ let
   inherit (config.my) username;
   archive = "/mnt/archive/${username}";
 in with config.users.users.${username}; {
-  imports =
-    [ "${inputs.unstable}/nixos/modules/hardware/opentabletdriver.nix" ];
+  imports = [
+    ./libvirt.nix
+    "${inputs.unstable}/nixos/modules/hardware/opentabletdriver.nix"
+  ];
 
   # Enable opentabletdriver.
   hardware.opentabletdriver = with pkgs; {
@@ -13,56 +15,8 @@ in with config.users.users.${username}; {
     package = unstable.opentabletdriver;
   };
 
-  boot = {
-    # Early load i195 for better resolution in init.
-    initrd.kernelModules = [ "i915" ];
-
-    # Do not load NVIDIA drivers.
-    blacklistedKernelModules = [ "nvidia" "nouveau" "hid-uclogic" ];
-
-    # Load VFIO related modules.
-    kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
-    extraModprobeConfig = "options vfio-pci ids=10de:1c02,10de:10f1";
-
-    # Enable IOMMU
-    kernelParams = [ "intel_iommu=on" ];
-  };
-
-  # Enable libvirtd.
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemuOvmf = true;
-      onBoot = "ignore";
-      onShutdown = "shutdown";
-      # Create symbolic links for /var/lib/libvirt/inputs/event-mouse and
-      # /var/lib/libvirt/inputs/event-kbd by probing the entries in either
-      # /dev/input/by-id or /dev/input/by-path.
-      qemuVerbatimConfig = ''
-        nographics_allow_host_audio = 1
-        cgroup_device_acl = [
-          "/dev/null", "/dev/full", "/dev/zero",
-          "/dev/random", "/dev/urandom",
-          "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
-          "/dev/rtc","/dev/hpet",
-          "/var/lib/libvirt/inputs/event-mouse",
-          "/var/lib/libvirt/inputs/event-kbd"
-        ]
-      '';
-    };
-  };
-
-  # Add user to libvirtd group.
-  users.users.${username} = { extraGroups = [ "libvirtd" ]; };
-
   # Some misc packages
-  environment.systemPackages = with pkgs; [
-    cpusetWithPatch
-    hdparm
-    rtorrent
-    samba
-    virtmanager
-  ];
+  environment.systemPackages = with pkgs; [ hdparm rtorrent samba ];
 
   services = {
     # Enable irqbalance service
@@ -163,41 +117,10 @@ in with config.users.users.${username}; {
   };
 
   networking = {
-    # Needs to disable global DHCP to use bridge interfaces
-    useDHCP = false;
-    interfaces.br0.useDHCP = true;
-
-    # Enable bridge.
-    bridges = {
-      br0 = {
-        interfaces = if config.networking.usePredictableInterfaceNames then
-          [ "eno1" ]
-        else
-          [ "eth0" ];
-      };
-    };
-
-    # Open ports to Samba.
+    # Open ports to Samba
     firewall = {
       allowedTCPPorts = [ 139 445 ];
       allowedUDPPorts = [ 137 138 ];
     };
-  };
-
-  # Reduce latency.
-  powerManagement.cpuFreqGovernor = "performance";
-
-  systemd.services.setupWin10Vm = {
-    after = [ "libvirtd.service" ];
-    requires = [ "libvirtd.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = "yes";
-    };
-    script = ''
-      uuid="$(${pkgs.libvirt}/bin/virsh domuuid win10 || true)"
-      ${pkgs.libvirt}/bin/virsh define <(sed "s/@UUID@/$uuid/" ${./win10.xml})
-    '';
   };
 }
