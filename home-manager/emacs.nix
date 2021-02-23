c7d4f9efd123c9b08674ca9c182c8fab943806b4
Thiago Kenji okada <thiagokokada@gmail.com>
2021-02-22 21:07:32 -0300

    home-manager/emacs: clean-up Emacs config
    Those are duplicated from my thiagokokada/dotfiles repo.

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -20,33 +20,4 @@
     enable = true;
     package = pkgs.emacs-custom;
   };
-
-  home.activation.clone-doom-emacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    [ ! -d "$HOME/.config/emacs" ] \
-      && "$DRY_RUN_CMD" ${pkgs.git}/bin/git clone 'https://github.com/hlissner/doom-emacs/' "$HOME/.config/emacs" \
-      || true
-  '';
-
-  home.sessionPath = [ "$HOME/.config/emacs/bin" ];
-
-  programs.zsh = {
-    shellAliases = {
-      em = "run-bg emacs";
-      et = "emacs -nw";
-    };
-    initExtra = ''
-      emp() {
-        local arg
-        for arg in $@; do
-          if [[ -d "$arg" ]]; then
-            touch "$arg/.projectile"
-          elif [[ -f "$arg" ]]; then
-            local dirname=$(dirname )
-            touch "$(dirname \"$arg\")/.projectile"
-          fi
-        done
-        em $@
-      }
-    '';
-  };
 }
