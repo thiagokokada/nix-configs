2ffcd5240626b401830c571e8c8cf1e6a36c6fc7
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-07-20 19:46:02 -0300

    Revert "nixos/desktop: enable systemd-resolved"
    This reverts commit 4e81ded459feb87401ac4173af89f5ecf7ab47bb.

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -6,17 +6,11 @@
   environment.systemPackages = with pkgs; [ smartmontools ];
 
   services = {
-    # Enable Gnome Keyring
-    gnome.gnome-keyring.enable = true;
-
-    # Enable systemd-resolved
-    resolved.enable = true;
-
-    # Enable SMART monitoring
     smartd = {
       enable = true;
       notifications.x11.enable = true;
     };
+    gnome.gnome-keyring.enable = true;
   };
 
   xdg = {
