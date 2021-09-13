b09eae12ebbc821fc408da6ba464dbfd44411c6e
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-09-13 14:17:41 -0300

    nixos/desktop: add scanning/printing support

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -1,11 +1,25 @@
 { pkgs, config, ... }:
 
+let
+  inherit (config.my) username;
+in
 {
   imports = [ ./audio.nix ];
 
-  environment.systemPackages = with pkgs; [ smartmontools ];
+  environment.systemPackages = with pkgs; [ smartmontools gnome.simple-scan ];
+
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.hplipWithPlugin ];
+  };
+
+  users.users.${username} = { extraGroups = [ "sane" "lp" ]; };
 
   services = {
+    printing = {
+      enable = true;
+      drivers = [ pkgs.hplip ];
+    };
     smartd = {
       enable = true;
       notifications.x11.enable = true;
