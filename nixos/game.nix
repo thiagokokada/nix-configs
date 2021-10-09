51f91b662ea19e8bbc82473d50821700138f3946
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-09 19:54:41 -0300

    nixos/game: steam tweaks

--- nixos/game.nix
+++ nixos/game.nix
@@ -1,6 +1,9 @@
 { pkgs, config, ... }:
 
 {
+  # Fix: MESA-INTEL: warning: Performance support disabled, consider sysctl dev.i915.perf_stream_paranoid=0
+  boot.kernelParams = [ "dev.i915.perf_stream_paranoid=0" ];
+
   environment.systemPackages = with pkgs; [
     piper
     unstable.lutris
@@ -8,7 +11,10 @@
   ];
 
   # Enable steam
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+  };
 
   # Enable ratbagd (for piper).
   services.ratbagd = {
