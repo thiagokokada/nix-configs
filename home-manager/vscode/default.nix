cd582c6995c25cece441a9f45d0dfbe91cb7a185
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-10 20:38:51 -0300

    home-manager/vscode: add more packages

--- home-manager/vscode/default.nix
+++ home-manager/vscode/default.nix
@@ -5,11 +5,21 @@
     enable = true;
     package = pkgs.unstable.vscodium;
     extensions = with pkgs.unstable.vscode-extensions; [
+      # Clojure
+      betterthantomorrow.calva
+
+      # Go
+      golang.go
+
+      # Python
+      # Broken until merged: https://github.com/NixOS/nixpkgs/pull/140564
+      # ms-python.python
+
       # Nix
       b4dm4n.vscode-nixpkgs-fmt
       bbenoist.nix
 
-      # VSpaceCode related
+      # VSpaceCode
       bodil.file-browser
       kahole.magit
       vscodevim.vim
@@ -21,6 +31,12 @@
         publisher = "jacobdufault";
         version = "0.0.3";
         sha256 = "sha256-oN1SzXypjpKOTUzPbLCTC+H3I/40LMVdjbW3T5gib0M=";
+      }
+      {
+        name = "rainbow-brackets";
+        publisher = "2gua";
+        version = "0.0.6";
+        sha256 = "sha256-TVBvF/5KQVvWX1uHwZDlmvwGjOO5/lXbgVzB26U8rNQ=";
       }
     ];
     userSettings = with builtins; fromJSON (readFile ./settings.json);
