135a5cc4a1f34b4f4dd8ac2ed33a737a8aa6ee22
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-12 01:36:19 -0300

    nixos/dev: switch to podman in place of docker

--- nixos/dev.nix
+++ nixos/dev.nix
@@ -23,9 +23,15 @@ in
   # Enable anti-aliasing in Java
   environment.variables._JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=gasp";
 
-  virtualisation.docker.enable = true;
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      dockerSocket.enable = true;
+    };
+  };
 
   # Added user to groups
-  users.users.${username}.extraGroups = [ "docker" "wireshark" ];
+  users.users.${username}.extraGroups = [ "podman" "wireshark" ];
 }
