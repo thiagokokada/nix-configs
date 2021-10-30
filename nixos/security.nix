43a253c847bdcd8e9c8ee0d27914c0b49a140850
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-30 00:20:04 -0300

    nixos/security: refactor, now for real

--- nixos/security.nix
+++ nixos/security.nix
@@ -1,51 +1,54 @@
 { pkgs, ... }:
 let
   # Mostly safe to block unless the service is doing something very strange
-  safeHardeningFlags = {
-    LockPersonality = true;
-    NoNewPrivileges = true;
-    PrivateTmp = true;
-    RestrictNamespaces = true;
-    RestrictRealtime = true;
-    SystemCallArchitectures = "native";
-  };
-  strictHardeningFlags = safeHardeningFlags // {
-    PrivateDevices = true;
-    ProtectClock = true;
-    ProtectControlGroups = true;
-    ProtectHome = true;
-    ProtectHostname = true;
-    ProtectKernelLogs = true;
-    ProtectKernelModules = true;
-    ProtectKernelTunables = true;
-    ProtectSystem = true;
-  };
-  restrictNetworkFlags = { RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6"; };
+  hardenFlags = { overrides ? { } }:
+    {
+      LockPersonality = true;
+      NoNewPrivileges = true;
+      PrivateDevices = true;
+      PrivateTmp = true;
+      ProtectClock = true;
+      ProtectControlGroups = true;
+      ProtectHostname = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      SystemCallArchitectures = "native";
+    } // overrides;
+  # Further hardening that may be applied depending on the case
+  PrivateNetwork = true; # Creates a private 'lo' interface
+  ProtectHome = true; # Protect /home from access
+  ProtectSystem = true; # Protect most of / from access
+  RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6"; # Restrict known address families
 in
 {
   # systemd-analyze --user security
   systemd.user.services = {
-    opentabletdriver.serviceConfig = safeHardeningFlags;
+    opentabletdriver.serviceConfig = hardenFlags { };
   };
 
   # systemd-analyze security
   systemd.services = {
-    flood.serviceConfig = safeHardeningFlags // restrictNetworkFlags // {
-      ProtectHome = false;
+    flood.serviceConfig = hardenFlags {
+      overrides = { inherit ProtectSystem RestrictAddressFamilies; };
     };
-    rtorrent.serviceConfig = strictHardeningFlags // restrictNetworkFlags // {
-      ProtectHome = false;
+    rtorrent.serviceConfig = hardenFlags {
+      overrides = { inherit ProtectSystem RestrictAddressFamilies; };
     };
-    plex.serviceConfig = safeHardeningFlags // {
-      RestrictNamespaces = false;
+    plex.serviceConfig = hardenFlags {
+      overrides = { RestrictNamespaces = false; };
     };
-    samba-nmbd.serviceConfig = safeHardeningFlags;
-    samba-smbd.serviceConfig = safeHardeningFlags;
-    samba-winbindd.serviceConfig = safeHardeningFlags;
-    smartd.serviceConfig = strictHardeningFlags // {
-      ProtectClock = false;
-      PrivateDevices = false;
-      PrivateNetwork = true;
+    samba-nmbd.serviceConfig = hardenFlags { };
+    samba-smbd.serviceConfig = hardenFlags { };
+    samba-winbindd.serviceConfig = hardenFlags { };
+    smartd.serviceConfig = hardenFlags {
+      overrides = {
+        inherit ProtectHome ProtectSystem PrivateNetwork;
+        ProtectClock = false;
+        PrivateDevices = false;
+      };
     };
   };
 
