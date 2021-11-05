f408f379c1eb2db5ad919a6678ae9b6d3b5011d4
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-11-04 23:43:43 -0300

    nix-darwin: remove nixUnstable workaround
    Now that nix 2.4 was released this shouldn't be a problem anymore.

--- nix-darwin/meta.nix
+++ nix-darwin/meta.nix
@@ -7,20 +7,12 @@ in
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
-  environment.systemPackages = [
-    (pkgs.writeScriptBin "nixFlakes" ''
-      exec ${pkgs.nixUnstable}/bin/nix --experimental-features "nix-command flakes" "$@"
-    '')
-  ];
-
   nix = {
     trustedUsers = [ "root" "@wheel" ];
     package = pkgs.nixFlakes;
-    # FIXME: doesn't seem to be working, so we are using nixFlakes instead
-    # https://github.com/LnL7/nix-darwin/issues/355
-    # extraOptions = ''
-    #   experimental-features = nix-command flakes
-    # '';
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
 
     # Set the $NIX_PATH entry for nixpkgs. This is necessary in
     # this setup with flakes, otherwise commands like `nix-shell
