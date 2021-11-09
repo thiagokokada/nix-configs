62ce551d28f9ef7200ec5c1784c8711c794168b2
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-11-08 22:47:37 -0300

    packages: clean-up arguments

--- packages/nixos-cleanup/default.nix
+++ packages/nixos-cleanup/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 pkgs.stdenv.mkDerivation {
   name = "nixos-cleanup";
--- packages/open-browser/default.nix
+++ packages/open-browser/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 let
   inherit (pkgs) python3Packages makeDesktopItem;
