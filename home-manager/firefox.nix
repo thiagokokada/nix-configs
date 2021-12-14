faab5fc062658bdf5aff66d4e6c2e9dbad385511
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-12-14 19:28:22 -0300

    home-manager/firefox: switch to firefox-bin

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -5,6 +5,9 @@ in
 {
   programs.firefox = {
     enable = true;
+    # Until this PR is merged use firefox-bin to avoid memory leak
+    # https://github.com/NixOS/nixpkgs/pull/148107
+    package = pkgs.firefox-bin;
     profiles.${username} = {
       settings = {
         # https://wiki.archlinux.org/title/Firefox#Hardware_video_acceleration
