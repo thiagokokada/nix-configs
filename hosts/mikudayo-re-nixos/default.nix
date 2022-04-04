5135eff45f024645c603681889043578f17503b6
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-04-04 22:32:55 +0100

    hosts/mikudayo-re-nixos: add nvidia GPU

--- hosts/mikudayo-re-nixos/default.nix
+++ hosts/mikudayo-re-nixos/default.nix
@@ -18,11 +18,22 @@ in
     ../../modules
     ../../overlays
     inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-gpu-nvidia
   ];
 
   device = {
     type = "notebook";
     netDevices = [ "enp3s0" "wlan0" ];
+  };
+
+  hardware.nvidia.prime = {
+    offload.enable = true;
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
   };
 
   # Use the systemd-boot EFI boot loader.
