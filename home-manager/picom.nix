e6ea8aae83e25c3dd64490b86def938ad1cb89db
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-04-11 12:19:13 +0100

    home-manager/picom: make backend conditional

--- home-manager/picom.nix
+++ home-manager/picom.nix
@@ -1,13 +1,20 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, super, ... }:
 
+let
+  videoDrivers = super.services.xserver.videoDrivers or [ ];
+  backend =
+    if (builtins.elem "nvidia" videoDrivers)
+    then "xrender"
+    else "glx";
+in
 {
   services.picom = {
+    inherit backend;
     enable = config.device.type != "vm";
     package = pkgs.unstable.picom-next;
     experimentalBackends = true;
     fade = true;
     fadeDelta = 2;
-    backend = "glx";
     vSync = true;
     extraOptions = ''
       unredir-if-possible = true;
