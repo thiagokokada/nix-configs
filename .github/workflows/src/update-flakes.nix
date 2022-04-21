afbc7ec5663639d0fbe2d1000ea52436083ea626
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-04-22 00:11:12 +0100

    github: fix update-flakes action

--- .github/workflows/src/update-flakes.nix
+++ .github/workflows/src/update-flakes.nix
@@ -4,12 +4,9 @@ let
 in
 {
   name = "update-flakes";
-  on = [
-    "workflow_dispatch"
-    {
-      schedule = [{ cron = "40 4 * * 0,2,4,6"; }];
-    }
-  ];
+  on = {
+    schedule = [{ cron = "40 4 * * 0,2,4,6"; }];
+  };
   jobs = {
     update-flakes = {
       inherit (constants.ubuntu) runs-on;
