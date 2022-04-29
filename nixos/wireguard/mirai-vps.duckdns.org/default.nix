494b0572597aa003af2125bfd01e1fa0b412e999
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-04-29 13:37:41 -0300

    nixos/wireguard: add pixel6

--- nixos/wireguard/mirai-vps.duckdns.org/default.nix
+++ nixos/wireguard/mirai-vps.duckdns.org/default.nix
@@ -2,6 +2,7 @@
 {
   # Generate with `wg-generate-config` script
   networking.wireguard.interfaces.${wgInterface}.peers = [
+    (import ./pixel6.nix)
     (import ./s20.nix)
     (import ./tabs8.nix)
   ];
