da16f6eb79fcbbedd68c71a9bbfc62b1eceb9b16
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-05-24 00:07:51 +0100

    nix-darwin/meta: comment nix settings since it is broken

--- nix-darwin/meta.nix
+++ nix-darwin/meta.nix
@@ -4,5 +4,7 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
-  nix = import ../shared/nix.nix { inherit pkgs self; };
+  # nix.settings is missing on nix-darwin
+  # https://github.com/LnL7/nix-darwin/issues/433
+  # nix = import ../shared/nix.nix { inherit pkgs self; };
 }
