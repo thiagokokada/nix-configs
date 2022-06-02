09b8c2755efa8debecc31c27904201493c511a96
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-06-02 17:44:52 +0100

    Revert "cachix: remove it"
    This reverts commit 7d433988ea9682d47fa3df143d480f22ee08ea53.

--- nixos/meta.nix
+++ nixos/meta.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ../cachix.nix
     ../overlays
   ];
 
--- shared/nix-conf.nix
+++ shared/nix-conf.nix
@@ -5,14 +5,4 @@
   # generate too much garbage
   # keep-outputs = true;
   # keep-derivations = true;
-
-  # cachix
-  extra-substituters = [
-    "https://nix-community.cachix.org"
-    "https://thiagokokada-nix-configs.cachix.org"
-  ];
-  extra-trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "thiagokokada-nix-configs.cachix.org-1:MwFfYIvEHsVOvUPSEpvJ3mA69z/NnY6LQqIQJFvNwOc="
-  ];
 }
