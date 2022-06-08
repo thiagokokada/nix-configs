09892ecb7de5756c61d7fd123e40c9a5061565e8
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-06-08 19:07:58 +0100

    home-manager/macos: bring the contents of home-manager/darwin to it

--- home-manager/macos.nix
+++ home-manager/macos.nix
@@ -2,10 +2,28 @@
 
 {
   imports = [
-    ./darwin.nix
     ./emacs
     ./minimal.nix
     ./mpv
     ./nnn.nix
   ];
+
+  targets.darwin.defaults = {
+    # Disable all automatic substitution
+    NSGlobalDomain = {
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+    };
+    # Do not write .DS_Store files outside macOS
+    com.apple.desktopservices = {
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
+    };
+    # Disable mouse acceleration
+    com.apple.mouse.scalling = -1;
+    # com.apple.trackpad.scalling = -1;
+  };
 }
