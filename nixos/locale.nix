32ec2caad247b9b1697e7caa8f3e34135f0dc953
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-06-28 23:13:46 +0100

    nixos/locale: remove br-abnt2 from default layout list

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -2,21 +2,21 @@
 
 {
   # Select internationalisation properties.
-  i18n = lib.mkDefault {
-    defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_CTYPE = "pt_BR.UTF-8"; # Fix ç in us-intl.
-      LC_TIME = "pt_BR.UTF-8";
-      LC_COLLATE = "C"; # Use C style string sort.
+      LC_CTYPE = lib.mkDefault "pt_BR.UTF-8"; # Fix ç in us-intl.
+      LC_TIME = lib.mkDefault "pt_BR.UTF-8";
+      LC_COLLATE = lib.mkDefault "C"; # Use C style string sort.
     };
   };
 
   # Set X11 keyboard layout.
-  services.xserver = lib.mkDefault {
-    layout = "us,br";
-    xkbVariant = "intl,abnt2";
+  services.xserver = {
+    layout = lib.mkDefault "us";
+    xkbVariant = lib.mkDefault "intl";
     # Remap Caps Lock to Esc, and use Super+Space to change layouts
-    xkbOptions = "caps:escape,grp:win_space_toggle";
+    xkbOptions = lib.mkDefault "caps:escape,grp:win_space_toggle";
   };
 
   # Set your time zone.
