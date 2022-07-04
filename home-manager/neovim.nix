afaba3ddb5820fb238533b52f32411cb75a6b839
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-04 19:03:29 +0100

    home-manager/neovim: add nvim-treesitter

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -242,6 +242,7 @@
       })
       auto-pairs
       gitgutter
+      (nvim-treesitter.withPlugins (plugins: unstable.tree-sitter.allGrammars))
       vim-automkdir
       vim-autoswap
       vim-better-whitespace
