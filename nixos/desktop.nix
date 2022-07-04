90a3058f27369fdc1235d42b46a9260eae7b481e
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-04 22:13:39 +0100

    nixos/desktop: convert it to module

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -1,36 +1,35 @@
-{ pkgs, config, ... }:
+{ config, lib, pkgs, ... }:
 
-let
-  inherit (config.meta) username;
-in
 {
-  imports = [ ./audio.nix ];
+  options.nixos.desktop.enable = lib.mkEnableOption "desktop config" // { default = true; };
 
-  environment.systemPackages = with pkgs; [ smartmontools ];
+  config = lib.mkIf config.nixos.desktop.enable {
+    environment.systemPackages = with pkgs; [ smartmontools ];
 
-  programs.gnome-disks.enable = true;
+    programs.gnome-disks.enable = true;
 
-  services = {
-    smartd = {
-      enable = true;
-      notifications.x11.enable = true;
+    services = {
+      smartd = {
+        enable = true;
+        notifications.x11.enable = true;
+      };
+      gnome = {
+        gnome-keyring.enable = true;
+        sushi.enable = true;
+      };
     };
-    gnome = {
-      gnome-keyring.enable = true;
-      sushi.enable = true;
-    };
-  };
 
-  xdg = {
-    # For sway screensharing
-    # https://nixos.wiki/wiki/Firefox
-    portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-wlr
-      ];
-      gtkUsePortal = true;
+    xdg = {
+      # For sway screensharing
+      # https://nixos.wiki/wiki/Firefox
+      portal = {
+        enable = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-wlr
+        ];
+        gtkUsePortal = true;
+      };
     };
   };
 }
