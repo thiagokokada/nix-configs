4c0548d83d3350542bf04626531868dc5d45a9ad
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-05 11:59:03 +0100

    home-manager/emacs: re-added et

--- home-manager/emacs/default.nix
+++ home-manager/emacs/default.nix
@@ -7,6 +7,7 @@
   home = {
     packages = with pkgs; [
       (run-bg-alias "em" "${config.programs.doom-emacs.package}/bin/emacs")
+      (writeShellScriptBin "et" "${config.programs.doom-emacs.package}/bin/emacs -nw $@")
       # font for my config
       fira-code
       hack-font
