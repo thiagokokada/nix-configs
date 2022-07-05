29e96195d1d4c2d805a9865995e30d4f03bd8b18
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-05 17:13:51 +0100

    home-manager/emacs: add tree-sitter overrides

--- home-manager/emacs/default.nix
+++ home-manager/emacs/default.nix
@@ -32,6 +32,10 @@
       if stdenv.isDarwin then
         emacsNativeComp
       else emacsPgtkNativeComp;
+    emacsPackagesOverlay = final: prev: {
+      ts-fold = prev.ts;
+      tree-sitter-langs = prev.tree-sitter-langs.override { plugins = pkgs.unstable.tree-sitter.allGrammars; };
+    };
     extraPackages = with pkgs; [
       fd
       findutils
