ab77a66a0a7a1af8d9e57f4732519716d22c3e11
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-15 22:03:14 +0100

    nixos/audio: simplify pipewire config

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -15,40 +15,6 @@
         support32Bit = true;
       };
       pulse.enable = true;
-      # Use realtime priority
-      config.pipewire = {
-        "context.modules" = [
-          {
-            name = "libpipewire-module-rtkit";
-            args = {
-              "nice.level" = -15;
-              "rt.prio" = 88;
-              "rt.time.soft" = 200000;
-              "rt.time.hard" = 200000;
-            };
-            flags = [ "ifexists" "nofail" ];
-          }
-          { name = "libpipewire-module-protocol-native"; }
-          { name = "libpipewire-module-profiler"; }
-          { name = "libpipewire-module-metadata"; }
-          { name = "libpipewire-module-spa-device-factory"; }
-          { name = "libpipewire-module-spa-node-factory"; }
-          { name = "libpipewire-module-client-node"; }
-          { name = "libpipewire-module-client-device"; }
-          {
-            name = "libpipewire-module-portal";
-            flags = [ "ifexists" "nofail" ];
-          }
-          {
-            name = "libpipewire-module-access";
-            args = { };
-          }
-          { name = "libpipewire-module-adapter"; }
-          { name = "libpipewire-module-link-factory"; }
-          { name = "libpipewire-module-session-manager"; }
-        ];
-      };
-
       wireplumber.enable = true;
     };
   };
