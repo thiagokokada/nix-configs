97504a80b174c7d36170a15731d9b9409d340514
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-08-22 22:02:21 +0100

    hosts/mikudayo-re-nixos: switch to xanmod_tt

--- hosts/mikudayo-re-nixos/default.nix
+++ hosts/mikudayo-re-nixos/default.nix
@@ -56,7 +56,7 @@ in
     timeZone = "Europe/Dublin";
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_tt;
   networking.hostName = "mikudayo-re-nixos";
 
   # I think the usage of NVIDIA drivers is making lidSwitchDocked not working correctly
