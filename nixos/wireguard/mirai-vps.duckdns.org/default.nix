5cc0413547e59bde40b5e8cc6a61d2156c492390
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-08-27 09:06:51 -0300

    nixos/wireguard: add mitab5 profile

--- nixos/wireguard/mirai-vps.duckdns.org/default.nix
+++ nixos/wireguard/mirai-vps.duckdns.org/default.nix
@@ -2,6 +2,7 @@
 {
   # Generate with `wg-generate-config` script
   networking.wireguard.interfaces.${wgInterface}.peers = [
+    (import ./mitab5.nix)
     (import ./pixel6.nix)
     (import ./s20.nix)
     (import ./tabs8.nix)
