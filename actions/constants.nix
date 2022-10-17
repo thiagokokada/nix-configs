ff9971e3bbe9f5439e34657e9ab39c5c1e0ce1c9
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-10-17 18:54:42 +0100

    actions: refactor it to not use Makefile

--- actions/constants.nix
+++ actions/constants.nix
@@ -1,4 +1,21 @@
 {
   ubuntu.runs-on = "ubuntu-latest";
   macos.runs-on = "macos-latest";
+  home-manager = {
+    linux.hostnames = [
+      "home-linux"
+      "steamdeck"
+    ];
+    darwin.hostnames = [
+      "home-macos"
+    ];
+  };
+  nixos.hostnames = [
+    "miku-nixos"
+    "mikudayo-re-nixos"
+    "mirai-vps"
+  ];
+  nix-darwin.hostnames = [
+    "miku-macos-vm"
+  ];
 }
