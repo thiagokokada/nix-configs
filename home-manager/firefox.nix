256eb8b129b0917d2da61c52bfdae8f018e4ac32
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-10-31 14:48:47 +0000

    home-manager/firefox: remove HW accel related settings

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -7,13 +7,7 @@ in
     enable = true;
     profiles.${username} = {
       settings = {
-        # https://wiki.archlinux.org/title/Firefox#Hardware_video_acceleration
-        "gfx.webrender.compositor" = true;
-        "gfx.x11-egl.force-enabled" = true;
         "browser.quitShortcut.disabled" = true;
-        "media.ffmpeg.vaapi.enabled" = true;
-        "media.ffvpx.enabled" = true;
-        "media.navigator.mediadatadecoder_vpx_enabled" = true;
       };
     };
   };
