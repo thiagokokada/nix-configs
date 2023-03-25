d7650ec7d3354dcb854fabd81c1e41beebddcba1
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-03-25 15:01:46 +0000

    home-manager/firefox: add more custom settings

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -7,7 +7,22 @@ in
     enable = true;
     profiles.${username} = {
       settings = {
+        # disable annoyinh Ctrl+Q shortcut
         "browser.quitShortcut.disabled" = true;
+        # enable hw video acceleration, if supported
+        "media.ffmpeg.vaapi.enabled" = true;
+        # handpicked settings from: https://github.com/arkenfox/user.js/blob/master/user.js
+        # ads
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "extensions.htmlaboutaddons.recommendations.enabled" = false;
+        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+        # telemetry
+        "datareporting.policy.dataSubmissionEnable" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "app.shield.optoutstudies.enabled" = false;
+        "app.normandy.enabled" = false;
+        "browser.tabs.crashReporting.sendReport" = false;
       };
     };
   };
