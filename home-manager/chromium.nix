169f40d6107c069f87ec24dffb1a1c3a40d2df05
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-04-13 20:13:41 +0100

    home-manager/chromium: add support for Wayland

--- home-manager/chromium.nix
+++ home-manager/chromium.nix
@@ -5,6 +5,8 @@
     enable = true;
     package = pkgs.chromium.override {
       enableWideVine = true;
+      # Needed for Wayland
+      commandLineArgs = "--ozone-platform-hint=auto";
     };
   };
 }
