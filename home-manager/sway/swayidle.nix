676b2f8e8fb9d6fd5b2cb0569524e756974cd35c
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-04-14 00:50:26 +0100

    home-manager/sway/sway-idle: fix syntax

--- home-manager/sway/swayidle.nix
+++ home-manager/sway/swayidle.nix
@@ -4,13 +4,13 @@
   services.swayidle = {
     enable = true;
     events = [
-      { event = "after-resume"; command = "${pkgs.sway}/bin/swaymsg 'output * dpms on'"; }
+      { event = "after-resume"; command = ''${pkgs.sway}/bin/swaymsg "output * dpms on"''; }
       { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
       { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
     ];
     timeouts = [
       { timeout = 600; command = "${pkgs.swaylock}/bin/swaylock -f"; }
-      { timeout = 605; command = "${pkgs.sway}/bin/swaymsg 'output * dpms off'"; }
+      { timeout = 605; command = ''${pkgs.sway}/bin/swaymsg "output * dpms off"''; }
     ];
   };
 
