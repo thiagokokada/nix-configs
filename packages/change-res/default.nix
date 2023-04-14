d357c3375ee03951a0cbccfff9014930a9ee7739
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-04-15 00:02:47 +0100

    package/change-res: use mons as fallback

--- packages/change-res/default.nix
+++ packages/change-res/default.nix
@@ -1,17 +1,18 @@
 { writeShellApplication
 , autorandr
+, mons
 , systemd
 }:
 
 writeShellApplication {
   name = "change-res";
-  runtimeInputs = [ autorandr systemd ];
+  runtimeInputs = [ autorandr mons systemd ];
   text = ''
     # Do not run this script in a Sway session
     if systemctl --quiet --user is-active sway-session.target; then
       exit 0
     fi
-    autorandr --change || autorandr common
+    autorandr --change || mons -o
     systemctl --user restart wallpaper.service
   '';
 }
