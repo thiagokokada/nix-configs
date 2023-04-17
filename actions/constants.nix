477c4f628b1b2721b0310c47f45fc07acb72c4bc
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-04-17 18:35:54 +0100

    actions: bump create-pull-request@v5

--- actions/constants.nix
+++ actions/constants.nix
@@ -4,7 +4,7 @@
     cachix-action = "cachix/cachix-action@v12";
     install-nix-action = "cachix/install-nix-action@v20";
     maximize-build-space = "easimon/maximize-build-space@v7";
-    create-pull-request = "peter-evans/create-pull-request@v4";
+    create-pull-request = "peter-evans/create-pull-request@v5";
     command-output = "mathiasvr/command-output@v2.0.0";
   };
   ubuntu.runs-on = "ubuntu-latest";
