d8d9503c194028c25d445957fe0ab5767419d33c
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-04-24 21:03:29 +0100

    packages/nix-cleanup: remove resholve

--- packages/nix-cleanup/default.nix
+++ packages/nix-cleanup/default.nix
@@ -1,52 +1,23 @@
-{ bash
+{ writeShellApplication
+, lib
 , coreutils
 , findutils
 , gnugrep
 , nix
-, resholve
 , substituteAll
 , isNixOS ? false
 }:
 
-resholve.mkDerivation rec {
-  pname =
+writeShellApplication {
+  name =
     if isNixOS
     then "nixos-cleanup"
     else "nix-cleanup";
 
-  version = "0.0.1";
-
-  src = (substituteAll {
+  text = lib.readFile (substituteAll {
     src = ./nix-cleanup.sh;
     isNixOS = if isNixOS then "1" else "0";
   });
 
-  dontUnpack = true;
-
-  installPhase = ''
-    runHook preInstall
-
-    install -Dm755 "$src" "$out/bin/${pname}"
-
-    runHook postInstall
-  '';
-
-  solutions = {
-    nix-whereis = {
-      scripts = [ "bin/${pname}" ];
-      interpreter = "${bash}/bin/bash";
-      inputs = [ coreutils findutils gnugrep nix ];
-      fake = {
-        external = [
-          "nixos-rebuild"
-          # https://github.com/abathur/resholve/issues/29
-          "sudo"
-        ];
-      };
-      execer = [
-        "cannot:${nix}/bin/nix-store"
-        "cannot:${nix}/bin/nix-collect-garbage"
-      ];
-    };
-  };
+  runtimeInputs = [ coreutils findutils gnugrep nix ];
 }
--- packages/nix-cleanup/nix-cleanup.sh
+++ packages/nix-cleanup/nix-cleanup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 readonly NIXOS=@isNixOS@
 AUTO=0
 OPTIMIZE=0
