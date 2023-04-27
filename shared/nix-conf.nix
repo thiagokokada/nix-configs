05a9d2edb3d27525b95a8a10fac8d457ba1dd196
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-04-27 16:47:49 +0100

    shared/nix-config: add some extra experimental features

--- shared/nix-conf.nix
+++ shared/nix-conf.nix
@@ -1,7 +1,7 @@
 {
   auto-optimise-store = true;
   trusted-users = [ "root" "@wheel" ];
-  experimental-features = [ "nix-command" "flakes" ];
+  experimental-features = [ "nix-command" "flakes" "impure-derivations" "ca-derivations" ];
   # Useful for nix-direnv, however not sure if this will
   # generate too much garbage
   # keep-outputs = true;
