aa79fea19d572f8224cda1b608bf6a65b0fd0373
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-04-30 10:49:14 +0100

    nixos/wayland: add xdg.portal

--- nixos/wayland.nix
+++ nixos/wayland.nix
@@ -3,4 +3,11 @@
 {
   # Enable PAM integration necessary for e.g.: swaylock
   programs.sway.enable = true;
+  xdg.portal = {
+    enable = true;
+    # Always use portal with xdg-open
+    xdgOpenUsePortal = true;
+    # Allow for screensharing in wlroots-based desktop
+    wlr.enable = true;
+  };
 }
