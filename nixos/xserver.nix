7c849e82bdedeeb2b7b8b6a69dbf4bcd238e286f
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-05-04 20:28:06 +0100

    Revert "Revert "nixos/xserver: use services.autorandr""
    This reverts commit f316bd3e0c700b7479df77a2f184c5dd2081d6cc.

--- nixos/xserver.nix
+++ nixos/xserver.nix
@@ -20,6 +20,7 @@
   };
 
   services = {
+    autorandr.enable = true;
     # Configure greetd, a lightweight session manager
     greetd = {
       enable = true;
@@ -49,19 +50,6 @@
       };
       vt = 7;
     };
-
-    # Configure monitor hotplug
-    udev.extraRules =
-      let
-        inherit (config.meta) username;
-        inherit (config.users.users.${username}) home;
-        inherit (config.services.greetd) vt;
-      in
-      ''
-        KERNEL=="card[0-9]*", SUBSYSTEM=="drm", ACTION=="change", ENV{DISPLAY}=":${toString vt}", \
-          ENV{HOME}="${home}", ENV{XAUTHORITY}="${home}/.local/share/sx/xauthority", \
-          RUN+="${pkgs.change-res}/bin/change-res"
-      '';
 
     xserver = {
       enable = true;
--- packages/change-res/default.nix
+++ packages/change-res/default.nix
@@ -8,11 +8,6 @@ writeShellApplication {
   name = "change-res";
   runtimeInputs = [ autorandr mons systemd ];
   text = ''
-    # Do not run this script in a Sway session
-    if systemctl --quiet --user is-active sway-session.target; then
-      echo "Sway session is active, skipping resolution change..."
-      exit 1
-    fi
     autorandr --change --default default
   '';
 }
