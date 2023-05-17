a17b625c05675596966c0eb907ef2653fd1efd12
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-05-17 22:32:01 +0100

    home-manager/sway/swayidle: add resumeCommand

--- home-manager/sway/swayidle.nix
+++ home-manager/sway/swayidle.nix
@@ -4,13 +4,29 @@
   services.swayidle = {
     enable = true;
     events = [
-      { event = "after-resume"; command = ''systemctl restart --user kanshi.service && ${pkgs.sway}/bin/swaymsg "output * dpms on"''; }
-      { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
-      { event = "lock"; command = "${pkgs.swaylock}/bin/swaylock -f"; }
+      {
+        event = "after-resume";
+        command = ''systemctl restart --user kanshi.service && ${pkgs.sway}/bin/swaymsg "output * dpms on"'';
+      }
+      {
+        event = "before-sleep";
+        command = "${pkgs.swaylock}/bin/swaylock -f";
+      }
+      {
+        event = "lock";
+        command = "${pkgs.swaylock}/bin/swaylock -f";
+      }
     ];
     timeouts = [
-      { timeout = 600; command = "${pkgs.swaylock}/bin/swaylock -f"; }
-      { timeout = 605; command = ''${pkgs.sway}/bin/swaymsg "output * dpms off"''; }
+      {
+        timeout = 600;
+        command = "${pkgs.swaylock}/bin/swaylock -f";
+      }
+      {
+        timeout = 605;
+        command = ''${pkgs.sway}/bin/swaymsg "output * dpms off"'';
+        resumeCommand = ''${pkgs.sway}/bin/swaymsg "output * dpms on"'';
+      }
     ];
   };
 
