77029de677450f2b5f5a30e940ac837fb2b84ac0
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-05-28 11:12:27 +0100

    home-manager/neovim: add lastplace_ignore_filetype

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -105,6 +105,7 @@
           lua << EOF
           require('nvim-lastplace').setup {
             lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+            lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
           }
           EOF
         '';
