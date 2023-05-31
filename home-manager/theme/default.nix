73004178a0987faef178210948c0bcf98322d46a
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-05-31 20:21:20 +0100

    home-manager/theme: disable ibus tray icon, fix option name

--- home-manager/theme/default.nix
+++ home-manager/theme/default.nix
@@ -76,8 +76,10 @@
 
   # https://github.com/GNOME/gsettings-desktop-schemas/blob/8527b47348ce0573694e0e254785e7c0f2150e16/schemas/org.gnome.desktop.interface.gschema.xml.in#L276-L296
   dconf.settings = lib.optionalAttrs (super ? fonts.fontconfig) {
+    # hide ibus systray icon, kinda buggy in waybar
+    "desktop/ibus/panel".show-icon-on-systray = false;
     "org/gnome/desktop/interface" = with super.fonts.fontconfig; {
-      "font-aliasing" =
+      "font-antialiasing" =
         if antialias then
           if (subpixel.rgba == "none")
           then "grayscale"
