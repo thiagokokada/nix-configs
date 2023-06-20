99695bf1f9473d695507bb19607176b31873eed0
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-06-20 21:36:36 +0100

    home-manager/sway/waybar: remove weekdays from calendar

--- home-manager/sway/waybar.nix
+++ home-manager/sway/waybar.nix
@@ -198,17 +198,15 @@ in
           format = " {:%H:%M, %a %d}";
           tooltip-format = "<tt><small>{calendar}</small></tt>";
           calendar = {
-            "mode" = "year";
-            "mode-mon-col" = 3;
-            "weeks-pos" = "right";
-            "on-scroll" = 1;
-            "on-click-right" = "mode";
-            "format" = {
-              "months" = "<span color='#ffead3'><b>{}</b></span>";
-              "days" = "<span color='#ecc6d9'><b>{}</b></span>";
-              "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
-              "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
-              "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            mode = "year";
+            mode-mon-col = 3;
+            on-scroll = 1;
+            on-click-right = "mode";
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
             };
           };
         };
