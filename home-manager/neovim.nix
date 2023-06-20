eea42882a537dea99678ac225d37a92661c0f59b
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-06-20 13:30:34 +0100

    home-manager/neovim: disable treesitter highlight for large buffers

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -266,6 +266,14 @@
           require('nvim-treesitter.configs').setup {
             highlight = {
               enable = true,
+              -- disable slow treesitter highlight for large files
+              disable = function(lang, buf)
+                  local max_filesize = 100 * 1024 -- 100 KB
+                  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                  if ok and stats and stats.size > max_filesize then
+                      return true
+                  end
+              end,
             },
             incremental_selection = {
               enable = true,
