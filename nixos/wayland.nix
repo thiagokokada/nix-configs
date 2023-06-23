52e4bd3fa2896788cdce2e2374e2f9b581ef255b
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-06-23 13:58:53 +0100

    nixos/wayland: remove `package = null` to show Sway in greetd

--- nixos/wayland.nix
+++ nixos/wayland.nix
@@ -5,7 +5,6 @@
     # Make Sway available for display managers
     enable = true;
     # Remove unnecessary packages from system-wide install (e.g.: foot)
-    package = null;
     extraPackages = [ ];
   };
 
