75eb3e54b3b2e90c449c9181de1cf722f8a54e9a
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-07-18 21:36:27 +0100

    nixos/cross-compiling: convert to module

--- nixos/cross-compiling.nix
+++ nixos/cross-compiling.nix
@@ -1,8 +1,20 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 {
-  # Allow compilation of packages ARM/ARM64 architectures via QEMU
-  # e.g. nix-build -A <pkg> --argstr system aarch64-linux
-  # https://nixos.wiki/wiki/NixOS_on_ARM#Compiling_through_QEMU
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" "armv7l-linux" ];
+  options.nixos.cross-compiling = {
+    enable = lib.mkDefaultOption "cross-compiling config for nixpkgs";
+    emulatedSystems = lib.mkOption {
+      description = "List of systems to emulate";
+      type = lib.types.listOf lib.types.str;
+      default = [ "aarch64-linux" ];
+    };
+  };
+  config = lib.mkIf config.nixos.cross-compiling.enable {
+    # Allow compilation of packages ARM/ARM64 architectures via QEMU
+    # e.g. nix-build -A <pkg> --argstr system aarch64-linux
+    # https://nixos.wiki/wiki/NixOS_on_ARM#Compiling_through_QEMU
+    boot.binfmt = {
+      inherit (config.nixos.cross-compiling) emulatedSystems;
+    };
+  };
 }
