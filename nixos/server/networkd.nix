7ed01bbe12275957245fc6069276c83121f9d7ea
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-09 21:30:28 -0300

    nixos/server/networkd: simplify config since useNetworkd is undeprecated

--- nixos/server/networkd.nix
+++ nixos/server/networkd.nix
@@ -10,19 +10,6 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.network = {
-      enable = true;
-      networks."10-wan" = {
-        matchConfig.Name = "en*";
-        networkConfig = {
-          # start a DHCP Client for IPv4 Addressing/Routing
-          DHCP = "ipv4";
-          # accept Router Advertisements for Stateless IPv6 Autoconfiguraton (SLAAC)
-          IPv6AcceptRA = true;
-        };
-        # make routing on this interface a dependency for network-online.target
-        linkConfig.RequiredForOnline = "routable";
-      };
-    };
+    networking.useNetworkd = true;
   };
 }
