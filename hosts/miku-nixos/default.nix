ce78c1d4dca258565926a23a88b50a7fa9467e56
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-17 11:52:49 +0100

    nixos/pc: remove
    Also remove my libvirt with GPU PCI-e passthrough configuration.

--- hosts/miku-nixos/default.nix
+++ hosts/miku-nixos/default.nix
@@ -13,7 +13,6 @@ in
     ../../nixos
     ../../nixos/cross-compiling.nix
     ../../nixos/games.nix
-    ../../nixos/pc.nix
     ../../nixos/security.nix
     inputs.hardware.nixosModules.common-cpu-intel
   ];
