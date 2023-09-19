c74ce3e1265a590ded5716faa82bfa1166a2b6f4
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-19 18:25:54 +0100

    hosts/mirai-vps: remove unnecessary inclusion of home-manager module

--- hosts/mirai-vps/default.nix
+++ hosts/mirai-vps/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ flake, ... }:
+{ ... }:
 
 {
   imports =
@@ -12,7 +12,6 @@
       ../../nixos/home.nix
       ../../nixos/minimal.nix
       ../../nixos/server
-      flake.inputs.home.nixosModules.home-manager
     ];
 
   nixos.home.imports = [
