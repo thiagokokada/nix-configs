64b1f0bb151631fdcef25890d99139e95f3d2339
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-20 15:05:14 +0100

    actions: bump versions

--- actions/constants.nix
+++ actions/constants.nix
@@ -1,8 +1,8 @@
 {
   actions = {
-    checkout = "actions/checkout@v3";
+    checkout = "actions/checkout@v4";
     cachix-action = "cachix/cachix-action@v12";
-    install-nix-action = "cachix/install-nix-action@v20";
+    install-nix-action = "cachix/install-nix-action@v22";
     maximize-build-space = "easimon/maximize-build-space@v7";
     create-pull-request = "peter-evans/create-pull-request@v5";
     command-output = "mathiasvr/command-output@v2.0.0";
