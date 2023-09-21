459f8b7d55952289d98f591399db18547853549b
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-21 16:03:00 +0100

    nixos/server/ssh: add more options

--- nixos/server/ssh.nix
+++ nixos/server/ssh.nix
@@ -1,11 +1,21 @@
 { config, lib, ... }:
 
-let inherit (config.meta) username;
+let
+  inherit (config.meta) username;
+  cfg = config.nixos.server.ssh;
 in
 {
-  options.nixos.server.ssh.enable = lib.mkEnableOption "SSH config";
+  options.nixos.server.ssh = {
+    enable = lib.mkEnableOption "SSH config";
+    enableRootLogin = lib.mkEnableOption "Root login via SSH";
+    authorizedKeys = lib.mkOption {
+      description = "List of authorized keys.";
+      type = lib.types.listOf lib.types.str;
+      default = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2emux6tprbzXmtykaW44sSd4o7e7E2wAWZMFBSUb87 thiagokokada@gmail.com" ];
+    };
+  };
 
-  config = lib.mkIf config.nixos.server.ssh.enable {
+  config = lib.mkIf cfg.enable {
     # Enable OpenSSH
     services = {
       fail2ban.enable = true;
@@ -23,8 +33,7 @@ in
     programs.mosh.enable = true;
 
     # Add SSH key
-    users.extraUsers.${username}.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2emux6tprbzXmtykaW44sSd4o7e7E2wAWZMFBSUb87 thiagokokada@gmail.com"
-    ];
+    users.users.root.openssh.authorizedKeys.keys = lib.mkIf cfg.enableRootLogin cfg.authorizedKeys;
+    users.extraUsers.${username}.openssh.authorizedKeys.keys = cfg.authorizedKeys;
   };
 }
