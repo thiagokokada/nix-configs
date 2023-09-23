211142deed323843b2af6679ae85cc37d1c11ea5
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-23 13:32:43 +0100

    Revert "nixos/nix/cross-compiling: do not enable QEMU emulation if Tailscale is enabled"
    This reverts commit 824a57ef99ca324ef67a0c914e7f3f905c7504f8.

--- nixos/nix/cross-compiling.nix
+++ nixos/nix/cross-compiling.nix
@@ -9,7 +9,7 @@ in
     emulatedSystems = lib.mkOption {
       description = "List of systems to emulate via QEMU";
       type = lib.types.listOf lib.types.str;
-      default = lib.optionals (!config.nixos.desktop.tailscale.enable) [ "aarch64-linux" ];
+      default = [ "aarch64-linux" ];
     };
   };
 
@@ -17,7 +17,7 @@ in
     # Allow compilation of packages ARM/ARM64 architectures via QEMU
     # e.g. nix-build -A <pkg> --argstr system aarch64-linux
     # https://nixos.wiki/wiki/NixOS_on_ARM#Compiling_through_QEMU
-    boot.binfmt = lib.mkIf (cfg.emulatedSystems != [ ]) {
+    boot.binfmt = {
       inherit (cfg) emulatedSystems;
     };
 
