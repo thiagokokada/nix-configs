97d56f5691e3891c1a67f51bb16a9d2a212becc1
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-28 21:08:22 +0100

    home-manager/minimal: fix build

--- home-manager/minimal.nix
+++ home-manager/minimal.nix
@@ -3,5 +3,8 @@
 {
   # Minimal set of packages to have a good CLI experience
   # Good starting point to start porting HM for new systems
-  imports = [ ./cli ];
+  imports = [
+    ./cli
+    ./meta
+  ];
 }
