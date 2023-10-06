cd5523663effc163037d0728437dc953140744e5
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-10-06 11:05:33 +0100

    shared/nix-config: remove impure-derivations/ca-derivations

--- shared/nix-conf.nix
+++ shared/nix-conf.nix
@@ -1,7 +1,7 @@
 {
   auto-optimise-store = true;
   trusted-users = [ "root" "@wheel" ];
-  experimental-features = [ "nix-command" "flakes" "impure-derivations" "ca-derivations" ];
+  experimental-features = [ "nix-command" "flakes" ];
   # Useful for nix-direnv, however not sure if this will
   # generate too much garbage
   # keep-outputs = true;
