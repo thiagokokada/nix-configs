cac37024f714187737a85027ab8b3f1fd903393b
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-10-10 12:50:06 +0100

    shared/nix: set nixPath/registry with the flake itself

--- shared/nix.nix
+++ shared/nix.nix
@@ -1,8 +1,5 @@
-{ pkgs, flake, ... }:
+{ flake, ... }:
 
-let
-  inherit (flake) inputs;
-in
 {
   # Enable Flakes
   settings = import ./nix-conf.nix;
@@ -11,11 +8,10 @@ in
   # this setup with flakes, otherwise commands like `nix-shell
   # -p pkgs.htop` will keep using an old version of nixpkgs
   nixPath = [
-    "nixpkgs=${inputs.nixpkgs}"
+    "nixpkgs=${flake}"
   ];
   # Same as above, but for `nix shell nixpkgs#htop`
-  # FIXME: for non-free packages you need to use `nix shell --impure`
   registry = {
-    nixpkgs.flake = inputs.nixpkgs;
+    nixpkgs.flake = flake;
   };
 }
