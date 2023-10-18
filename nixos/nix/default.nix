502d9cb6c3c6f104ca8077cb98224d98d41a34d8
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-10-18 14:59:14 +0100

    nixos/nix-diff: init

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -6,6 +6,7 @@ in
 {
   imports = [
     ./cross-compiling.nix
+    ./nix-diff.nix
   ];
 
   options.nixos.nix = {
---
+++ nixos/nix/nix-diff.nix
@@ -0,0 +1,21 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.nixos.nix.diff;
+in
+{
+  options.nixos.nix.diff.enable = lib.mkEnableOption "diff configuration on activation" // {
+    default = config.nixos.nix.enable;
+  };
+
+  config = lib.mkIf cfg.enable {
+    system.activationScripts.diff = ''
+      if [[ -e /run/current-system ]]; then
+        echo "showing changes compared to /run/current-system..."
+        ${lib.getExe' config.nix.package "nix"} \
+          --extra-experimental-features 'nix-command' \
+          store diff-closures /run/current-system "$systemConfig" || true
+      fi
+    '';
+  };
+}
