567eeaf9064828d95f0f8b0d363f2dae350b9635
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-10-18 13:41:03 +0100

    nixos/server: remove my own nixos-rebuild wrapper
    Now that https://github.com/NixOS/nixpkgs/pull/258571 is merged.

--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
 {
   options.nixos.server.enable = lib.mkEnableOption "server config" // {
     default = config.device.type == "server";
@@ -28,21 +28,5 @@
       allowReboot = true;
       flake = "github:thiagokokada/nix-configs";
     };
-    environment.systemPackages = with pkgs; [
-      # Run nixos-rebuild inside a systemd-run to avoid TTY closing issues
-      # https://github.com/NixOS/nixpkgs/issues/39118
-      (writeShellScriptBin "nixos-rebuild" ''
-        if [[ -z "$NIXOS_REBUILD_DIRTY_ENV" ]] && [[ -z "$SSH_CLIENT" ]] && [[ "$#" -ne 0 ]]; then
-          systemd-run \
-                -E NIX_PATH \
-                -E LOCALE_ARCHIVE \
-                -E PATH \
-                --unit=nixos-rebuild --pty --wait --collect --same-dir --service-type=exec --quiet \
-                "${nixos-rebuild}/bin/nixos-rebuild" "$@"
-        else
-          "${nixos-rebuild}/bin/nixos-rebuild" "$@"
-        fi
-      '')
-    ];
   };
 }
