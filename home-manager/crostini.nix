c6f78ed2c8c3e6b32eba464b3f6ee0ae36b9e551
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-04 13:41:11 +0000

    home-manager/crostini: update environrment variable

--- home-manager/crostini.nix
+++ home-manager/crostini.nix
@@ -21,7 +21,7 @@
     xdg.configFile."systemd/user/cros-garcon.service.d/override.conf".text = ''
       [Service]
       Environment="PATH=%h/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/local/games:/usr/sbin:/usr/bin:/usr/games:/sbin:/bin"
-      Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:/usr/local/share:/usr/share"
+      Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:%h/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
     '';
   };
 }
