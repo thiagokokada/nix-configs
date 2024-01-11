9d12afe8ea86e392bb860a51a9d6e9062ac55c09
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-11 18:09:45 +0000

    home-manager/wezterm: refactor config

--- home-manager/desktop/wezterm.nix
+++ home-manager/desktop/wezterm.nix
@@ -10,49 +10,52 @@
       enable = true;
       extraConfig = with config.home-manager.desktop.theme.fonts.symbols; /* lua */''
         local act = wezterm.action
-        return {
-          window_background_opacity = 0.9,
-          font = wezterm.font("${name}"),
-          font_size = 12.0,
-          color_scheme = "Builtin Pastel Dark",
-          hide_tab_bar_if_only_one_tab = true,
-          scrollback_lines = 10000,
-          audible_bell = "Disabled",
-          mouse_bindings = {
-            -- Change the default click behavior so that it only selects
-            -- text and doesn't open hyperlinks
-            {
-              event = { Up = { streak = 1, button = 'Left' } },
-              mods = 'NONE',
-              action = act.CompleteSelection 'ClipboardAndPrimarySelection',
-            },
-            -- Bind 'Up' event of CTRL-Click to open hyperlinks
-            {
-              event = { Up = { streak = 1, button = 'Left' } },
-              mods = 'CTRL',
-              action = act.OpenLinkAtMouseCursor,
-            },
-            -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
-            {
-              event = { Down = { streak = 1, button = 'Left' } },
-              mods = 'CTRL',
-              action = act.Nop,
-            },
-            -- Scrolling up while holding CTRL increases the font size
-            {
-              event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-              mods = 'CTRL',
-              action = act.IncreaseFontSize,
-            },
+        local config = wezterm.config_builder()
 
-            -- Scrolling down while holding CTRL decreases the font size
-            {
-              event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-              mods = 'CTRL',
-              action = act.DecreaseFontSize,
-            },
+        config.audible_bell = "Disabled"
+        config.color_scheme = "Builtin Pastel Dark"
+        config.enable_kitty_keyboard = true
+        config.font = wezterm.font("${name}")
+        config.font_size = 12.0
+        config.hide_tab_bar_if_only_one_tab = true
+        config.scrollback_lines = 10000
+        config.window_background_opacity = 0.9
+        config.mouse_bindings = {
+          -- Change the default click behavior so that it only selects
+          -- text and doesn't open hyperlinks
+          {
+            event = { Up = { streak = 1, button = 'Left' } },
+            mods = 'NONE',
+            action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+          },
+          -- Bind 'Up' event of CTRL-Click to open hyperlinks
+          {
+            event = { Up = { streak = 1, button = 'Left' } },
+            mods = 'CTRL',
+            action = act.OpenLinkAtMouseCursor,
+          },
+          -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+          {
+            event = { Down = { streak = 1, button = 'Left' } },
+            mods = 'CTRL',
+            action = act.Nop,
+          },
+          -- Scrolling up while holding CTRL increases the font size
+          {
+            event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+            mods = 'CTRL',
+            action = act.IncreaseFontSize,
+          },
+
+          -- Scrolling down while holding CTRL decreases the font size
+          {
+            event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+            mods = 'CTRL',
+            action = act.DecreaseFontSize,
           },
         }
+
+        return config
       '';
     };
   };
