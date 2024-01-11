204879bd56f2645057ba8ccff2c8e71f6d681eab
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-11 18:13:50 +0000

    home-manager/wezterm: overwrite colors (but not the colorscheme)

--- home-manager/desktop/wezterm.nix
+++ home-manager/desktop/wezterm.nix
@@ -8,18 +8,30 @@
   config = lib.mkIf config.home-manager.desktop.wezterm.enable {
     programs.wezterm = {
       enable = true;
-      extraConfig = with config.home-manager.desktop.theme.fonts.symbols; /* lua */''
+      extraConfig = let
+        inherit (config.home-manager.desktop.theme) fonts colors;
+      in
+      with colors; /* lua */''
         local act = wezterm.action
         local config = wezterm.config_builder()
 
         config.audible_bell = "Disabled"
         config.color_scheme = "Builtin Pastel Dark"
         config.enable_kitty_keyboard = true
-        config.font = wezterm.font("${name}")
+        config.font = wezterm.font("${fonts.symbols.name}")
         config.font_size = 12.0
         config.hide_tab_bar_if_only_one_tab = true
         config.scrollback_lines = 10000
         config.window_background_opacity = 0.9
+        config.colors = {
+          foreground = "${base05}",
+          background = "${base00}",
+          cursor_bg = "${base05}",
+          cursor_border = "${base05}",
+          cursor_fg = "${base00}",
+          selection_bg = "${base02}",
+          selection_fg = "${base05}",
+        };
         config.mouse_bindings = {
           -- Change the default click behavior so that it only selects
           -- text and doesn't open hyperlinks
