384529e43ae3e9fb59b4ab94e82fd6d8ba7d0e23
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-02-05 18:28:14 +0000

    home-manager/neovim: fix syntax highlighting of Lua code

--- home-manager/editor/neovim.nix
+++ home-manager/editor/neovim.nix
@@ -3,6 +3,7 @@
 let
   devCfg = config.home-manager.dev;
   cfg = config.home-manager.editor.neovim;
+  toLuaBool = x: if x then "true" else "false";
 in
 {
   options.home-manager.editor.neovim = {
@@ -209,12 +210,13 @@ in
             local actions = require('lir.actions')
             local mark_actions = require('lir.mark.actions')
             local clipboard_actions = require('lir.clipboard.actions')
+            local enable_icons = ${toLuaBool cfg.enableIcons}
 
             require('lir').setup {
               show_hidden_files = false,
               ignore = {},
               devicons = {
-                enable = ${if cfg.enableIcons then "true" else "false"},
+                enable = enable_icons,
                 highlight_dirname = false,
               },
               mappings = {
@@ -258,9 +260,11 @@ in
           type = "lua";
           # TODO: add support for trailing whitespace
           config = /* lua */ ''
+            local enable_icons = ${toLuaBool cfg.enableIcons}
+
             require('lualine').setup {
               options = {
-                icons_enabled = ${if cfg.enableIcons then "true" else "false"},
+                icons_enabled = enable_icons,
               },
             }
           '';
@@ -479,11 +483,11 @@ in
             -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
             local lspconfig = require('lspconfig')
 
-            ${lib.optionalString devCfg.enable ''
+            ${lib.optionalString devCfg.enable /* lua */ ''
               lspconfig.bashls.setup {}
               lspconfig.marksman.setup {}
             ''}
-            ${lib.optionalString devCfg.nix.enable ''
+            ${lib.optionalString devCfg.nix.enable /* lua */ ''
               lspconfig.nil_ls.setup {
                 settings = {
                   ['nil'] = {
@@ -494,17 +498,17 @@ in
                 },
               }
             ''}
-            ${lib.optionalString devCfg.clojure.enable ''
+            ${lib.optionalString devCfg.clojure.enable /* lua */ ''
               lspconfig.clojure_lsp.setup {}
             ''}
-            ${lib.optionalString devCfg.go.enable ''
+            ${lib.optionalString devCfg.go.enable /* lua */ ''
               lspconfig.gopls.setup {}
             ''}
-            ${lib.optionalString devCfg.python.enable ''
+            ${lib.optionalString devCfg.python.enable /* lua */ ''
               lspconfig.pyright.setup {}
               lspconfig.ruff_lsp.setup {}
             ''}
-            ${lib.optionalString devCfg.node.enable ''
+            ${lib.optionalString devCfg.node.enable /* lua */''
               lspconfig.cssls.setup {}
               lspconfig.eslint.setup {}
               lspconfig.html.setup {}
