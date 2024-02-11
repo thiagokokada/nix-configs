1afaca637319c88a55fbf0ceb844b0af39ac3ee4
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-02-11 19:26:08 +0000

    actions: downgrade install-nix-action

--- actions/constants.nix
+++ actions/constants.nix
@@ -4,7 +4,7 @@
     checkout = "actions/checkout@v4";
     create-pull-request = "peter-evans/create-pull-request@v6";
     free-disk-space = "jlumbroso/free-disk-space@v1.3.1";
-    install-nix-action = "cachix/install-nix-action@v25";
+    install-nix-action = "cachix/install-nix-action@v23";
   };
   ubuntu.runs-on = "ubuntu-latest";
   macos.runs-on = "macos-13";
