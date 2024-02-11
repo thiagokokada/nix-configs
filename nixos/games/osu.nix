ef19fe1214dea25e6448b5aef13de363fd06c3e3
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-02-11 19:39:25 +0000

    nixos/osu: disable it for now

--- nixos/games/osu.nix
+++ nixos/games/osu.nix
@@ -4,9 +4,7 @@ let
   cfg = config.nixos.games.osu;
 in
 {
-  options.nixos.games.osu.enable = lib.mkEnableOption "osu! config" // {
-    default = config.nixos.games.enable;
-  };
+  options.nixos.games.osu.enable = lib.mkEnableOption "osu! config";
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; let
