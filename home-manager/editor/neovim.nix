53d93df7d81ad7470ba0e964e153837f65e5b59f
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-02-24 11:27:06 +0000

    home-manager/neovim: lir-nvim -> oil-nvim

--- home-manager/editor/neovim.nix
+++ home-manager/editor/neovim.nix
@@ -208,59 +208,6 @@ in
           '';
         }
         {
-          plugin = lir-nvim;
-          type = "lua";
-          config = /* lua */ ''
-            -- disable netrw
-            vim.g.loaded_netrw = 1
-            vim.g.loaded_netrwPlugin = 1
-
-            local actions = require('lir.actions')
-            local mark_actions = require('lir.mark.actions')
-            local clipboard_actions = require('lir.clipboard.actions')
-            local enable_icons = ${toLuaBool cfg.enableIcons}
-
-            require('lir').setup {
-              show_hidden_files = false,
-              ignore = {},
-              devicons = {
-                enable = enable_icons,
-                highlight_dirname = false,
-              },
-              mappings = {
-                ['<Enter>'] = actions.edit,
-                ['<C-s>'] = actions.split,
-                ['<C-v>'] = actions.vsplit,
-                ['<C-t>'] = actions.tabedit,
-
-                ['-'] = actions.up,
-                ['q'] = actions.quit,
-
-                ['K'] = actions.mkdir,
-                ['N'] = actions.newfile,
-                ['R'] = actions.rename,
-                ['@'] = actions.cd,
-                ['Y'] = actions.yank_path,
-                ['.'] = actions.toggle_show_hidden,
-                ['D'] = actions.delete,
-
-                ['J'] = function()
-                  mark_actions.toggle_mark()
-                  vim.cmd('normal! j')
-                end,
-                ['C'] = clipboard_actions.copy,
-                ['X'] = clipboard_actions.cut,
-                ['P'] = clipboard_actions.paste,
-              },
-            }
-
-            -- vinegar
-            vim.keymap.set('n', '-', function()
-              vim.cmd.edit(vim.fn.expand('%:p:h'))
-            end, { desc = "Files" })
-          '';
-        }
-        {
           plugin = lualine-nvim;
           type = "lua";
           config = /* lua */ ''
@@ -320,6 +267,21 @@ in
           type = "lua";
           config = /* lua */ ''
             require("nvim-surround").setup {}
+          '';
+        }
+        {
+          plugin = oil-nvim;
+          type = "lua";
+          config = /* lua */ ''
+            require("oil").setup {
+              default_file_explorer = true,
+              skip_confirm_for_simple_edits = true,
+              constrain_cursor = "name",
+              lsp_rename_autosave = true,
+              experimental_watch_for_changes = true,
+            }
+
+            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
           '';
         }
         {
