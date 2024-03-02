92734ef739f8abefddadbb4f76e3562167372f68
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-03-02 11:40:13 +0000

    nixos/nix: fix conflict

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -45,15 +45,6 @@ in
       # Leave nix builds as a background task
       daemonCPUSchedPolicy = "batch";
 
-      # Set the $NIX_PATH entry for nixpkgs. This is necessary in
-      # this setup with flakes, otherwise commands like `nix-shell
-      # -p pkgs.htop` will keep using an old version of nixpkgs
-      nixPath = [
-        "nixpkgs=${flake.inputs.nixpkgs}"
-      ];
-      # Same as above, but for `nix shell nixpkgs#htop`
-      registry.nixpkgs.flake = flake;
-
       settings =
         let
           substituters = import ../../shared/substituters.nix;
