780e1369b416763573f1d0b0247d02c1f1ef1c04
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-03-02 12:00:16 +0000

    nixos/audio: use toLua generator

--- nixos/desktop/audio/default.nix
+++ nixos/desktop/audio/default.nix
@@ -27,16 +27,20 @@ in
         pulse.enable = true;
         wireplumber = {
           enable = true;
-          configPackages = [
-            (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" /* lua */ ''
-              bluez_monitor.properties = {
-                ["bluez5.enable-sbc-xq"] = true,
-                ["bluez5.enable-msbc"] = true,
-                ["bluez5.enable-hw-volume"] = true,
-                ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-              }
-            '')
-          ];
+          configPackages =
+            let
+              properties = lib.generators.toLua { } {
+                "bluez5.enable-sbc-xq" = true;
+                "bluez5.enable-msbc" = true;
+                "bluez5.enable-hw-volume" = true;
+                "bluez5.headset-roles" = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]";
+              };
+            in
+            [
+              (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" /* lua */ ''
+                bluez_monitor.properties = ${properties}
+              '')
+            ];
         };
       };
     };
