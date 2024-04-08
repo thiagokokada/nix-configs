c096cac576532f545ab9f4a8ad9f86b3cb125b25
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-04-08 17:41:47 +0100

    treewide: stateVersion = 24.05

--- nixos/nix/default.nix
+++ nixos/nix/default.nix
@@ -76,6 +76,6 @@ in
     # compatible, in order to avoid breaking some software such as database
     # servers. You should change this only after NixOS release notes say you
     # should.
-    system.stateVersion = lib.mkDefault "23.11"; # Did you read the comment?
+    system.stateVersion = lib.mkDefault "24.05"; # Did you read the comment?
   };
 }
