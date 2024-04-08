69d802023b06e7f5e75f68242b93100452ba3997
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-04-08 14:43:46 +0100

    lib/attrsets: fix documentation

--- lib/attrsets.nix
+++ lib/attrsets.nix
@@ -5,7 +5,7 @@
 
     nix-repl> recursiveMergeAttrs [ { a = "foo"; } { b = "bar"; } ];
     { a = "foo"; b = "bar"; }
-    nix-repl> mergeAttrsetsList [ { a.b = "foo"; } { a.c = "bar"; } ]
+    nix-repl> recursiveMergeAttrs [ { a.b = "foo"; } { a.c = "bar"; } ]
     { a = { b = "foo"; c = "bar"; }; }
   */
   recursiveMergeAttrs = builtins.foldl' lib.recursiveUpdate { };
