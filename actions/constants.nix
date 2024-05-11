771663151848865ae834292f00c6b617cdabbff5
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-05-11 15:47:25 +0100

    actions: use cachix/install-nix-action@v26

--- actions/constants.nix
+++ actions/constants.nix
@@ -4,7 +4,7 @@
     checkout = "actions/checkout@v4";
     create-pull-request = "peter-evans/create-pull-request@v6";
     free-disk-space = "jlumbroso/free-disk-space@v1.3.1";
-    install-nix-action = "cachix/install-nix-action@v23";
+    install-nix-action = "cachix/install-nix-action@v26";
   };
   ubuntu.runs-on = "ubuntu-latest";
   macos.runs-on = "macos-13";
