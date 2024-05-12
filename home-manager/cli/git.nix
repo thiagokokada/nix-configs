8508ffdc89a2f261f20ab1435af646a51192d11d
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-05-12 18:56:37 +0100

    Revert "home-manager/git: switch to difftastic"
    This reverts commit 67bd65bd6c501a46c66d0b8e2885ca87788f2cfd.

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -62,9 +62,29 @@ in
               }
           else git;
 
-        difftastic = {
+        delta = {
           enable = true;
-          background = "dark";
+          options = {
+            features = "side-by-side line-numbers decorations";
+            syntax-theme = "Dracula";
+            plus-style = ''syntax "#003800"'';
+            minus-style = ''syntax "#3f0001"'';
+            decorations = {
+              commit-decoration-style = "bold yellow box ul";
+              file-style = "bold yellow ul";
+              file-decoration-style = "none";
+              hunk-header-decoration-style = "cyan box ul";
+            };
+            delta = {
+              navigate = true;
+            };
+            line-numbers = {
+              line-numbers-left-style = "cyan";
+              line-numbers-right-style = "cyan";
+              line-numbers-minus-style = 124;
+              line-numbers-plus-style = 28;
+            };
+          };
         };
 
         aliases = {
