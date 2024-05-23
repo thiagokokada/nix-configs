e374017ec8e8f011769f0cbf10cece15cc9e5d08
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-05-23 18:28:48 +0100

    home-manager/neovim: ruff-lsp -> ruff server

--- home-manager/editor/neovim.nix
+++ home-manager/editor/neovim.nix
@@ -663,8 +663,23 @@ in
               },
               { "clojure_lsp" },
               { "gopls" },
-              { "pyright" },
-              { "ruff_lsp" },
+              { "pyright",
+                opts = {
+                  settings = {
+                    pyright = {
+                      -- Using Ruff's import organizer
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                      analysis = {
+                        -- Ignore all files for analysis to exclusively use Ruff for linting
+                          ignore = { '*' },
+                      },
+                    },
+                  },
+                },
+              },
+              { "ruff" },
               { "cssls" },
               { "eslint" },
               { "html" },
