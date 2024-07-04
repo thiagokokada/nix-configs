fa84ec1d3181d75f0365f1b9a47ce03881dc363e
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-04 23:50:08 +0100

    home-manager/helix: enable soft-wrap

--- home-manager/editor/helix.nix
+++ home-manager/editor/helix.nix
@@ -12,14 +12,17 @@
       settings = {
         theme = "tokyonight";
 
-        editor.statusline = {
-          left = [ "mode" "spinner" ];
-          center = [ "file-name" ];
-          right = [ "diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type" ];
-          separator = "│";
-          mode.normal = "NORMAL";
-          mode.insert = "INSERT";
-          mode.select = "SELECT";
+        editor = {
+          soft-wrap.enable = true;
+          statusline = {
+            left = [ "mode" "spinner" ];
+            center = [ "file-name" ];
+            right = [ "diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type" ];
+            separator = "│";
+            mode.normal = "NORMAL";
+            mode.insert = "INSERT";
+            mode.select = "SELECT";
+          };
         };
 
         keys = {
