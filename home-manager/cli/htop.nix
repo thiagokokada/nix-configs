df63cd4cddd0cf58a396e2fb9b26705e701d5a5f
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-04 18:35:11 -0300

    home-manager/htop: remove settings

--- home-manager/cli/htop.nix
+++ home-manager/cli/htop.nix
@@ -9,10 +9,6 @@
     programs.htop = {
       enable = true;
       package = pkgs.htop-vim;
-      settings = {
-        hide_userland_threads = true;
-        color_scheme = 6;
-      };
     };
   };
 }
