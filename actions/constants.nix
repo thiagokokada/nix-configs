bac9999228bb17b035c843fb24f87f548fd63ff1
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-08 23:46:00 +0100

    actions: bump actions

--- actions/constants.nix
+++ actions/constants.nix
@@ -1,10 +1,15 @@
 {
   actions = {
-    cachix-action = "cachix/cachix-action@v14";
+    # https://github.com/marketplace/actions/cachix
+    cachix-action = "cachix/cachix-action@v15";
+    # https://github.com/marketplace/actions/checkout
     checkout = "actions/checkout@v4";
+    # https://github.com/marketplace/actions/create-pull-request
     create-pull-request = "peter-evans/create-pull-request@v6";
+    # https://github.com/marketplace/actions/free-disk-space-ubuntu
     free-disk-space = "jlumbroso/free-disk-space@v1.3.1";
-    install-nix-action = "cachix/install-nix-action@v26";
+    # https://github.com/marketplace/actions/install-nix
+    install-nix-action = "cachix/install-nix-action@v27";
   };
   ubuntu.runs-on = "ubuntu-latest";
   macos.runs-on = "macos-latest";
