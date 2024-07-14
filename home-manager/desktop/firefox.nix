16386746fada90e7ec4dd662b2dc62a107b6e950
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-14 01:21:04 +0100

    home-manager/firefox: add more custom settings

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -27,7 +27,9 @@ in
       profiles.${username} = {
         settings =
           {
-            # disable annoyinh Ctrl+Q shortcut
+            # disable about:config warning
+            "browser.aboutConfig.showWarning" = false;
+            # disable annoying Ctrl+Q shortcut
             "browser.quitShortcut.disabled" = true;
             # don't mess up with paste
             "dom.event.clipboardevents.enabled" = false;
@@ -39,12 +41,29 @@ in
             "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
             "extensions.htmlaboutaddons.recommendations.enabled" = false;
             "browser.urlbar.suggest.quicksuggest.sponsored" = false;
-            # telemetry
+            # data reporting
             "datareporting.policy.dataSubmissionEnable" = false;
             "datareporting.healthreport.uploadEnabled" = false;
+            # telemetry
+            "toolkit.telemetry.unified" = false;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.archive.enabled" = false;
+            "toolkit.telemetry.newProfilePing.enabled" = false;
+            "toolkit.telemetry.shutdownPingSender.enabled" = false;
+            "toolkit.telemetry.updatePing.enabled" = false;
+            "toolkit.telemetry.bhrPing.enabled" = false;
+            "toolkit.telemetry.firstShutdownPing.enabled" = false;
+            "toolkit.telemetry.coverage.opt-out" = true;
+            "toolkit.coverage.opt-out" = true;
+            "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+            "browser.newtabpage.activity-stream.telemetry" = false;
+            # studies
             "app.shield.optoutstudies.enabled" = false;
             "app.normandy.enabled" = false;
+            # crash report
             "browser.tabs.crashReporting.sendReport" = false;
+            # privacy
+            "dom.private-attribution.submission.enabled" = false;
           }
           // lib.optionalAttrs cfg.subpixelRender.enable {
             # https://pandasauce.org/get-fonts-done/
