ed8028fe5755fb38159b7d3a7b06c85a6a788db6
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-20 00:53:05 +0100

    home-manager/nixgl: add wezterm

--- home-manager/desktop/nixgl.nix
+++ home-manager/desktop/nixgl.nix
@@ -3,11 +3,18 @@
   lib,
   libEx,
   pkgs,
+  flake,
   ...
 }:
 
 let
   cfg = config.home-manager.desktop.nixgl;
+  nixGLWrapper' =
+    pkg:
+    libEx.nixGLWrapper pkgs {
+      inherit pkg;
+      nixGL = cfg.package;
+    };
 in
 {
   options.home-manager.desktop.nixgl = {
@@ -28,24 +35,15 @@ in
         # This may "overwrite" some of the personalizations from the
         # home-manager.desktop.firefox module, since the nixGLWrapper is
         # incompatible with it and we are prioritizing the nixGL wrapped binary
-        (lib.hiPrio (
-          libEx.nixGLWrapper pkgs {
-            pkg = firefox;
-            nixGL = cfg.package;
-          }
-        ))
+        (lib.hiPrio (nixGLWrapper' firefox))
       ];
 
     # Needs Vapoursynth disabled so we don't wrap the package
     home-manager.desktop.mpv.vapoursynth.enable = false;
 
     programs = {
-      mpv.package = lib.mkForce (
-        libEx.nixGLWrapper pkgs {
-          pkg = pkgs.mpv;
-          nixGL = cfg.package;
-        }
-      );
+      mpv.package = lib.mkForce (nixGLWrapper' pkgs.mpv);
+      wezterm.package = lib.mkForce (nixGLWrapper' pkgs.wezterm);
     };
   };
 }
