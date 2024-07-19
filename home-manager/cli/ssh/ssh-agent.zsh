e45a888f2bf3ce5644c3966f0b6371414d0291e2
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-19 01:18:43 +0100

    home-manager/ssh: refacto ssh-agent.zsh

--- home-manager/cli/ssh/ssh-agent.zsh
+++ home-manager/cli/ssh/ssh-agent.zsh
@@ -1,8 +1,16 @@
+zmodload zsh/net/socket
+
+_check_agent(){
+  if [[ -S "$SSH_AUTH_SOCK" ]] && zsocket "$SSH_AUTH_SOCK" 2>/dev/null; then
+    return 0
+  fi
+  return 1
+}
+
 _start_agent() {
   # Test if $SSH_AUTH_SOCK is visible, in case we start e.g.: ssh-agent via
   # systemd service
-  zmodload zsh/net/socket
-  if [[ -S "$SSH_AUTH_SOCK" ]] && zsocket "$SSH_AUTH_SOCK" 2>/dev/null; then
+  if _check_agent; then
     return 0
   fi
 
@@ -13,8 +21,8 @@ _start_agent() {
   if [[ -f "$ssh_env_cache" ]]; then
     source "$ssh_env_cache" > /dev/null
 
-    # Test if $SSH_AUTH_SOCK is visible
-    if [[ -S "$SSH_AUTH_SOCK" ]] && zsocket "$SSH_AUTH_SOCK" 2>/dev/null; then
+    # Test if $SSH_AUTH_SOCK is visible, e.g.: the ssh-agent is still alive
+    if _check_agent; then
       return 0
     fi
   fi
@@ -28,4 +36,4 @@ _start_agent() {
 }
 
 _start_agent
-unfunction _start_agent
+unfunction _check_agent _start_agent
