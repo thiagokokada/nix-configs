176c0107cad04cace0f021d09faae8c8042e8412
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-19 19:11:56 +0100

    packages/run-bg-alias: rewrite using daemonize

--- packages/run-bg-alias/default.nix
+++ packages/run-bg-alias/default.nix
@@ -1,46 +1,18 @@
 {
-  stdenv,
-  coreutils,
   writeShellApplication,
-  command,
+  daemonize,
+  which,
   name,
+  command,
 }:
 
 writeShellApplication {
   inherit name;
-  runtimeInputs = [ coreutils ];
-  text =
-    # macOS seems to lose the current PWD for some reason if you
-    # close stdin, but without closing stdin you can't have a proper
-    # background application.
-    # This function should make it by expanding the paths, but it
-    # is using a heuristic so it may be buggy.
-    if stdenv.isDarwin then
-      ''
-        declare -a parsed_args
-
-        while [[ "$#" -gt 0 ]]; do
-          arg="$1"
-          if [[ -d "$arg" ]] || [[ -f "$arg" ]]; then
-            parsed_args+=("$(realpath "$arg")")
-          else
-            parsed_args+=("$arg")
-          fi
-          shift
-        done
-
-        exec 0>&-
-        exec 1>&-
-        exec 2>&-
-        ${command} "''${parsed_args[@]}" &
-        disown $!
-      ''
-    else
-      ''
-        exec 0>&-
-        exec 1>&-
-        exec 2>&-
-        ${command} "$@" &
-        disown $!
-      '';
+  runtimeInputs = [
+    daemonize
+    which
+  ];
+  text = ''
+    daemonize -c "$PWD" "$(which "${command}")" "$@"
+  '';
 }
