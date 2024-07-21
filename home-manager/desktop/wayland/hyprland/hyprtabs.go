62f53f0e48b8fff01c0fe837caea2957f613ec92
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-21 16:53:48 +0100

    home-manager/hyprland: fix usage of hyprland-ipc-client

--- home-manager/desktop/wayland/hyprland/hyprtabs.go
+++ home-manager/desktop/wayland/hyprland/hyprtabs.go
@@ -23,14 +23,12 @@ func main() {
 	c := client.MustClient(os.Getenv("HYPRLAND_INSTANCE_SIGNATURE"))
 	awindow := must1(c.ActiveWindow())
 	batchArgs := client.NewByteQueue()
-	// https://github.com/labi-le/hyprland-ipc-client/issues/7
-	batchArgs.Add([]byte("noop;"))
 
 	if len(awindow.Grouped) > 0 {
 		// If we are already in a group, ungroup
-		batchArgs.Add([]byte("dispatch togglegroup;"))
+		batchArgs.Add([]byte("togglegroup"))
 		// Make the current window as master (when using master layout)
-		batchArgs.Add([]byte("dispatch layoutmsg swapwithmaster master;"))
+		batchArgs.Add([]byte("layoutmsg swapwithmaster master"))
 	} else {
 		aworkspace := must1(c.ActiveWorkspace())
 		clients := must1(c.Clients())
@@ -44,7 +42,7 @@ func main() {
 		}
 
 		// Start by creating a new group
-		batchArgs.Add([]byte("dispatch togglegroup;"))
+		batchArgs.Add([]byte("togglegroup"))
 		for _, w := range windows {
 			// Move each window inside the group
 			// Once is not enough in case of very "deep" layouts,
@@ -53,16 +51,23 @@ func main() {
 			// For master layouts we also call swapwithmaster, this
 			// makes the switch more reliable
 			for i := 0; i < 1; i++ {
-				batchArgs.Add([]byte(fmt.Sprintf("dispatch focuswindow address:%s;", w)))
-				batchArgs.Add([]byte("dispatch layoutmsg swapwithmaster auto;"))
-				batchArgs.Add([]byte("dispatch moveintogroup l;"))
-				batchArgs.Add([]byte("dispatch moveintogroup r;"))
-				batchArgs.Add([]byte("dispatch moveintogroup u;"))
-				batchArgs.Add([]byte("dispatch moveintogroup d;"))
+				batchArgs.Add([]byte(fmt.Sprintf("focuswindow address:%s", w)))
+				batchArgs.Add([]byte("layoutmsg swapwithmaster auto"))
+				batchArgs.Add([]byte("moveintogroup l"))
+				batchArgs.Add([]byte("moveintogroup r"))
+				batchArgs.Add([]byte("moveintogroup u"))
+				batchArgs.Add([]byte("moveintogroup d"))
 			}
+			// Thanks to limitations to hyprland-ipc-client, we
+			// will dispatch the current commands to avoid hitting
+			// the command limit
+			// https://github.com/labi-le/hyprland-ipc-client/issues/7#issuecomment-2241632277
+			response := must1(c.Dispatch(batchArgs))
+			fmt.Printf("Response for window=[%s]: %s\n", w, response)
+			batchArgs = client.NewByteQueue()
 		}
 		// Focus in the active window at the end
-		batchArgs.Add([]byte(fmt.Sprintf("dispatch focuswindow address:%s;", awindow.Address)))
+		batchArgs.Add([]byte(fmt.Sprintf("focuswindow address:%s", awindow.Address)))
 	}
 	fmt.Printf("# of Commands: %d\n", batchArgs.Len())
 	// Run in batch for performance
--- home-manager/desktop/wayland/hyprland/hyprtabs.nix
+++ home-manager/desktop/wayland/hyprland/hyprtabs.nix
@@ -6,7 +6,7 @@ buildGoModule {
 
   src = ./.;
 
-  vendorHash = "sha256-vMKHN84/dypVbjyRQBzhWhhqCPenR+FSjvKGeDf+kM4=";
+  vendorHash = "sha256-fO9UpFtFZMa4m84XuKyCO5r+7gt7N9om21cVwjAibQw=";
 
   ldflags = [
     "-s"
