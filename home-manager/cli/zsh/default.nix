6fff3b99809a4b40421f42c553a2c96d17ba8b67
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-08-01 00:10:38 +0100

    home-manager/zsh: make realise-symlink support directories

--- home-manager/cli/zsh/default.nix
+++ home-manager/cli/zsh/default.nix
@@ -18,8 +18,17 @@ let
     text = ''
       for file in "$@"; do
         if [[ -L "$file" ]]; then
-          cp --verbose --remove-destination "$(readlink "$file")" "$file"
-          chmod --changes +w "$file"
+          if [[ -d "$file" ]]; then
+            tmpdir="''${file}.tmp"
+            mkdir -p "$tmpdir"
+            cp --verbose --recursive "$file"/* "$tmpdir"
+            unlink "$file"
+            mv "$tmpdir" "$file"
+            chmod --changes --recursive +w "$file"
+          else
+            cp --verbose --remove-destination "$(readlink "$file")" "$file"
+            chmod --changes +w "$file"
+          fi
         else
           >&2 echo "Not a symlink: $file"
           exit 1
