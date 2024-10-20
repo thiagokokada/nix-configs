d9b369b68a49958cdfb1f5ffe07549b490c7e125
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-10-20 12:22:25 +0100

    Revert "nixos/tailscale: remove workaround for NixOS/nixpkgs #180175"
    This reverts commit d29ba6bb9bfe827db8e54be74279e6a2523fac26.

--- nixos/desktop/tailscale.nix
+++ nixos/desktop/tailscale.nix
@@ -10,5 +10,9 @@
       enable = true;
       useRoutingFeatures = if config.nixos.server.tailscale.enable then "both" else "client";
     };
+
+    # Disable wait online as it's causing trouble at rebuild
+    # See: https://github.com/NixOS/nixpkgs/issues/180175
+    systemd.services.NetworkManager-wait-online.enable = false;
   };
 }
