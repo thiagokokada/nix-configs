f4b373a50ade0d927c97604ab8f65860d38ba76a
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-02-10 15:34:52 +0000

    home-manager/neovim: RTFM to make mini-surround closer to vim-surround

--- modules/home-manager/editor/neovim.nix
+++ modules/home-manager/editor/neovim.nix
@@ -318,12 +318,28 @@ in
                 require('mini.pairs').setup {}
                 require('mini.surround').setup {
                   mappings = {
-                    add = 'ys',
-                    delete = 'ds',
-                    replace = 'cs',
+                    add = "ys",
+                    delete = "ds",
+                    find = "",
+                    find_left = "",
+                    highlight = "",
+                    replace = "cs",
+                    update_n_lines = "",
+
+                    -- Add this only if you don't want to use extended mappings
+                    suffix_last = "",
+                    suffix_next = "",
                   },
                   n_lines = 100,
+                  search_method = "cover_or_next",
                 }
+
+                -- Remap adding surrounding to Visual mode selection
+                vim.keymap.del('x', 'ys')
+                vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+                -- Make special mapping for "add surrounding for line"
+                vim.keymap.set('n', 'yss', 'ys_', { remap = true })
 
                 local miniclue = require('mini.clue')
                 miniclue.setup {
