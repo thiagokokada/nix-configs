94c63a9b41907f25b1c720b1af1eb07dcf9830db
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-03-12 11:23:03 +0000

    home-manager/git: remove delta
    Delta can be really slow in some cases and also its diff make it
    difficult to copy something when needed.
    
    Let's rely on the good ol' diff instead.

--- modules/home-manager/cli/git.nix
+++ modules/home-manager/cli/git.nix
@@ -70,31 +70,6 @@ in
           else
             git;
 
-        delta = {
-          enable = true;
-          options = {
-            features = "side-by-side line-numbers decorations";
-            syntax-theme = "Dracula";
-            plus-style = ''syntax "#003800"'';
-            minus-style = ''syntax "#3f0001"'';
-            decorations = {
-              commit-decoration-style = "bold yellow box ul";
-              file-style = "bold yellow ul";
-              file-decoration-style = "none";
-              hunk-header-decoration-style = "cyan box ul";
-            };
-            delta = {
-              navigate = true;
-            };
-            line-numbers = {
-              line-numbers-left-style = "cyan";
-              line-numbers-right-style = "cyan";
-              line-numbers-minus-style = 124;
-              line-numbers-plus-style = 28;
-            };
-          };
-        };
-
         aliases = {
           branch-default = ''!git symbolic-ref --short refs/remotes/origin/HEAD | sed "s|^origin/||"'';
           checkout-default = ''!git checkout "$(git branch-default)"'';
