960c70fdc2d237906e2543339840b2b1dc54c7db
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-03-12 18:56:15 +0000

    nixos/watchdogd: simplify code

--- modules/nixos/server/watchdogd.nix
+++ modules/nixos/server/watchdogd.nix
@@ -6,41 +6,34 @@
 }:
 let
   cfg = config.nixos.server.watchdogd;
-  ping = pkgs.writeShellScriptBin "ping-watchdog" ''
+  ping-watchdog = pkgs.writeShellScriptBin "ping-watchdog" ''
     /run/current-system/sw/bin/ping -c 4 8.8.8.8
   '';
+  logDirectory = "/var/lib/misc";
 in
 {
   options.nixos.server.watchdogd = {
     enable = lib.mkEnableOption "watchdogd config" // {
       default = config.nixos.server.enable;
     };
-    logDirectory = lib.mkOption {
-      type = lib.types.path;
-      description = "Log directory";
-      default = "/var/lib/misc";
-    };
   };
 
   config = lib.mkIf cfg.enable {
-    # Since we are using unsafeDiscardStringContext, we need to reference this
-    # path somewhere else, otherwise the script may be GC'd
-    environment.systemPackages = [ ping ];
+    environment.systemPackages = [ ping-watchdog ];
 
     services.watchdogd = {
       enable = true;
       settings = {
-        # Strings with context are not supported as keys in attrsets
-        "generic ${builtins.unsafeDiscardStringContext ping}/bin/ping-watchdog" = {
+        "generic /run/current-system/sw/bin/ping-watchdog" = {
           enabled = true;
           interval = 300;
           timeout = 60;
           critical = 1;
         };
-        reset-reason.file = "${cfg.logDirectory}/watchdogd.state";
+        reset-reason.file = "${logDirectory}/watchdogd.state";
       };
     };
 
-    systemd.tmpfiles.rules = [ "d ${cfg.logDirectory} 0700" ];
+    systemd.tmpfiles.rules = [ "d ${logDirectory} 0700" ];
   };
 }
