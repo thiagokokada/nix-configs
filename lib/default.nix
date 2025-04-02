a71de23728c204b7222b5c804216aea59b8e4142
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-04-02 01:27:19 +0100

    lib: format fix

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,4 @@
 { nixpkgs, ... }@inputs:
 
 # lib should avoid depending on pkgs
-import ./attrsets.nix { inherit (nixpkgs) lib; }
-// import ./flake-helpers.nix inputs
+import ./attrsets.nix { inherit (nixpkgs) lib; } // import ./flake-helpers.nix inputs
