484fd88dce707461ff011e019e9ff4eed75b405c
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-06-10 11:39:26 +0100

    home-manager/asdf: format

--- modules/home-manager/dev/asdf.nix
+++ modules/home-manager/dev/asdf.nix
@@ -12,9 +12,12 @@
 
   config = lib.mkIf config.home-manager.dev.asdf.enable {
     home = {
-      activation.reshimAsdf = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        run ${lib.getExe pkgs.asdf-vm} reshim
-      '';
+      activation.reshimAsdf =
+        lib.hm.dag.entryAfter [ "writeBoundary" ]
+          # bash
+          ''
+            run ${lib.getExe pkgs.asdf-vm} reshim
+          '';
 
       file.".asdfrc".text = lib.generators.toKeyValue { } {
         legacy_version_file = "yes";
