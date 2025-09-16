151225606908183cbb2b442a0f065a13a2f271bd
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-09-16 10:15:32 +0100

    nixos/ollama: add network-online.target for ollama-model-loader

--- modules/nixos/dev/ollama.nix
+++ modules/nixos/dev/ollama.nix
@@ -26,5 +26,10 @@
         openFirewall = true;
       };
     };
+
+    systemd.services.ollama-model-loader = {
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+    };
   };
 }
