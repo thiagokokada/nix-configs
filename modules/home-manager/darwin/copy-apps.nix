74b149a76dc88a3d4bf64e215ccc3c501f7c2e36
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-09-17 18:24:08 +0100

    home-manager/copy-apps: bring improvements from @PedroHLC/system-setup

--- modules/home-manager/darwin/copy-apps.nix
+++ modules/home-manager/darwin/copy-apps.nix
@@ -17,44 +17,55 @@ in
 
   config = lib.mkIf cfg.enable {
     # https://github.com/nix-darwin/nix-darwin/pull/1396#issuecomment-2908304517
+    # https://github.com/PedroHLC/system-setup/blob/6f41b5e8740da7fbe85d37c0d15f8fe464d4bbc0/home-configurations/foreign/trampolines.nix
     home.activation.copyApplications =
       let
-        targetFolder = "${config.home.homeDirectory}/Applications/Home Manager Apps";
-        homeApps = pkgs.buildEnv {
-          name = "home-applications";
+        applications = pkgs.buildEnv {
+          name = "user-applications";
           paths = config.home.packages;
           pathsToLink = "/Applications";
         };
       in
-      lib.hm.dag.entryAfter [ "installPackages" ] ''
-        # Set up home applications.
-        echo "Setting up ${targetFolder}..." >&2
+      lib.hm.dag.entryAfter [ "installPackages" ]
+        # bash
+        ''
+          set -e
 
-        # Clean up old style symlinks
-        if [ -e "${targetFolder}" ] && [ -L "${targetFolder}" ]; then
-          rm "${targetFolder}"
-        fi
-        mkdir -p "${targetFolder}"
+          targetFolder=${lib.strings.escapeShellArg config.targets.darwin.linkApps.directory}
 
-        rsyncFlags=(
-          # mtime is standardized in the nix store, which would leave only file size to distinguish files.
-          # Thus we need checksums, despite the speed penalty.
-          --checksum
-          # Converts all symlinks pointing outside of the copied tree (thus unsafe) into real files and directories.
-          # This neatly converts all the symlinks pointing to application bundles in the nix store into
-          # real directories, without breaking any relative symlinks inside of application bundles.
-          # This is good enough, because the make-symlinks-relative.sh setup hook converts all $out internal
-          # symlinks to relative ones.
-          --copy-unsafe-links
-          --archive
-          --delete
-          # https://github.com/nix-community/home-manager/issues/1341#issuecomment-3081211229
-          # --chmod=-w
-          --no-group
-          --no-owner
-        )
-        ${lib.getExe pkgs.rsync} "''${rsyncFlags[@]}" ${homeApps}/Applications/ "${targetFolder}"
-      '';
+          echo "setting up ~/$targetFolder..." >&2
+
+          ourLink () {
+            local link
+            link=$(readlink "$1")
+            [ -L "$1" ] && [ "''${link#*-}" = "home-manager-files/$targetFolder" ]
+          }
+
+          if [ -e "$targetFolder" ] && ourLink "$targetFolder"; then
+            rm "$targetFolder"
+          fi
+
+          mkdir -p "$targetFolder"
+
+          rsyncFlags=(
+            --archive
+            # mtime is standardized in the nix store, which would leave only file size to distinguish files.
+            # Thus we need checksums, despite the speed penalty.
+            --checksum
+            # Converts all symlinks pointing outside of the copied tree (thus unsafe) into real files and directories.
+            # This neatly converts all the symlinks pointing to application bundles in the nix store into
+            # real directories, without breaking any relative symlinks inside of application bundles.
+            # This is good enough, because the make-symlinks-relative.sh setup hook converts all $out internal
+            # symlinks to relative ones.
+            --copy-unsafe-links
+            --delete
+            --exclude=$'Icon\r'
+            --no-group
+            --no-owner
+          )
+
+          ${lib.getExe pkgs.rsync} "''${rsyncFlags[@]}" ${applications}/Applications/ "$targetFolder"
+        '';
 
     # Disable old style linking of applications in home-manager
     targets.darwin.linkApps.enable = false;
