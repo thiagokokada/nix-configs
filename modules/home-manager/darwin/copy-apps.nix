0b3134993ea37c90f11341b6680580f60bfccc13
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-09-17 15:23:50 +0100

    home-manager/copy-apps: backport some fixes from nix-darwin

--- modules/home-manager/darwin/copy-apps.nix
+++ modules/home-manager/darwin/copy-apps.nix
@@ -19,33 +19,41 @@ in
     # https://github.com/nix-darwin/nix-darwin/pull/1396#issuecomment-2908304517
     home.activation.copyApplications =
       let
-        targetDir = "${config.home.homeDirectory}/Applications/Home Manager Apps";
+        targetFolder = "${config.home.homeDirectory}/Applications/Home Manager Apps";
         homeApps = pkgs.buildEnv {
           name = "home-applications";
           paths = config.home.packages;
           pathsToLink = "/Applications";
         };
       in
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      lib.hm.dag.entryAfter [ "installPackages" ] ''
         # Set up home applications.
-        echo "Setting up ${targetDir}..." >&2
+        echo "Setting up ${targetFolder}..." >&2
 
         # Clean up old style symlinks
-        if [ -e "${targetDir}" ] && [ -L "${targetDir}" ]; then
-          rm "${targetDir}"
+        if [ -e "${targetFolder}" ] && [ -L "${targetFolder}" ]; then
+          rm "${targetFolder}"
         fi
-        mkdir -p "${targetDir}"
+        mkdir -p "${targetFolder}"
 
         rsyncFlags=(
+          # mtime is standardized in the nix store, which would leave only file size to distinguish files.
+          # Thus we need checksums, despite the speed penalty.
           --checksum
+          # Converts all symlinks pointing outside of the copied tree (thus unsafe) into real files and directories.
+          # This neatly converts all the symlinks pointing to application bundles in the nix store into
+          # real directories, without breaking any relative symlinks inside of application bundles.
+          # This is good enough, because the make-symlinks-relative.sh setup hook converts all $out internal
+          # symlinks to relative ones.
           --copy-unsafe-links
           --archive
           --delete
-          --chmod=-w
+          # https://github.com/nix-community/home-manager/issues/1341#issuecomment-3081211229
+          # --chmod=-w
           --no-group
           --no-owner
         )
-        ${lib.getExe pkgs.rsync} "''${rsyncFlags[@]}" ${homeApps}/Applications/ "${targetDir}"
+        ${lib.getExe pkgs.rsync} "''${rsyncFlags[@]}" ${homeApps}/Applications/ "${targetFolder}"
       '';
 
     # Disable old style linking of applications in home-manager
