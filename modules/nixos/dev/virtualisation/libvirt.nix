935dfba3a6efdc90718980731288c2350419c945
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-09-25 01:00:26 +0100

    nixos/libvirt: remove specialisation

--- modules/nixos/dev/virtualisation/libvirt.nix
+++ modules/nixos/dev/virtualisation/libvirt.nix
@@ -32,40 +32,38 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    specialisation.libvirt.configuration = {
-      # Very likely we want the desktop session if we are using libvirt
-      nixos.games.jovian.bootInDesktopMode = true;
+    # Very likely we want the desktop session if we are using libvirt
+    nixos.games.jovian.bootInDesktopMode = true;
 
-      boot.kernelParams = lib.optionals (cfg.vfioPci != [ ]) [
-        "vfio-pci.ids=${lib.concatStringsSep "," cfg.vfioPci.ids}"
-      ];
+    boot.kernelParams = lib.optionals (cfg.vfioPci != [ ]) [
+      "vfio-pci.ids=${lib.concatStringsSep "," cfg.vfioPci.ids}"
+    ];
 
-      environment.systemPackages = with pkgs; [
-        # https://wiki.nixos.org/wiki/Libvirt#Default_networking
-        dnsmasq
-        iommu-script
-      ];
+    environment.systemPackages = with pkgs; [
+      # https://wiki.nixos.org/wiki/Libvirt#Default_networking
+      dnsmasq
+      iommu-script
+    ];
 
-      programs.virt-manager.enable = true;
+    programs.virt-manager.enable = true;
 
-      virtualisation = {
-        libvirtd = {
-          enable = true;
-          qemu = {
-            package = pkgs.qemu_kvm;
-            runAsRoot = true;
-            swtpm.enable = true;
-            ovmf = {
-              enable = true;
-              packages = with pkgs; [ OVMFFull.fd ];
-            };
+    virtualisation = {
+      libvirtd = {
+        enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = true;
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = with pkgs; [ OVMFFull.fd ];
           };
         };
-
-        spiceUSBRedirection.enable = true;
       };
 
-      users.users.${username}.extraGroups = [ "libvirtd" ];
+      spiceUSBRedirection.enable = true;
     };
+
+    users.users.${username}.extraGroups = [ "libvirtd" ];
   };
 }
