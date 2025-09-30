328262bda82ca6f3324559096cf54013cd29790e
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-09-30 19:30:49 +0100

    hosts/penguin: move host specific settings from home-manager.crostini

--- hosts/home-manager/penguin/default.nix
+++ hosts/home-manager/penguin/default.nix
@@ -1,5 +1,10 @@
 {
   home.stateVersion = "24.05";
 
-  home-manager.crostini.enable = true;
+  home-manager = {
+    crostini.enable = true;
+    cli.git.gui.enable = true;
+    desktop.mpv.enable = true;
+    dev.enable = true;
+  };
 }
--- modules/home-manager/crostini.nix
+++ modules/home-manager/crostini.nix
@@ -4,12 +4,6 @@
   options.home-manager.crostini.enable = lib.mkEnableOption "Crostini (ChromeOS) config";
 
   config = lib.mkIf config.home-manager.crostini.enable {
-    home-manager = {
-      cli.git.gui.enable = true;
-      desktop.mpv.enable = true;
-      dev.enable = true;
-    };
-
     nixGL = {
       defaultWrapper = "mesa";
       installScripts = [ "mesa" ];
