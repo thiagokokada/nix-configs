e1db49dad0f62fb4fb7054818b1e873b51569278
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-10-13 18:26:33 +0100

    nix-darwin/home: remove copyApps via nix-darwin

--- modules/nix-darwin/home.nix
+++ modules/nix-darwin/home.nix
@@ -2,7 +2,6 @@
   config,
   flake,
   lib,
-  pkgs,
   ...
 }:
 
@@ -18,20 +17,8 @@ in
 
   config = lib.mkIf cfg.enable {
     nix-darwin.home.extraModules = {
-      home-manager.darwin.copyApps.enable = false;
       targets.darwin.linkApps.enable = false;
     };
-
-    # https://github.com/nix-community/home-manager/issues/1341#issuecomment-3256894180
-    system.build.applications = lib.mkForce (
-      pkgs.buildEnv {
-        name = "system-applications";
-        pathsToLink = "/Applications";
-        paths =
-          config.environment.systemPackages
-          ++ (lib.concatMap (x: x.home.packages) (lib.attrsets.attrValues config.home-manager.users));
-      }
-    );
 
     users.users.${username}.home = lib.mkDefault "/Users/${username}";
   };
