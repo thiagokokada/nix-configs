83dbf33dec686610c26d27fd0809c10fdd9c5c39
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-11-01 14:50:23 +0000

    home-manager/zsh: fix zsh-fast-syntax-highlighting path

--- modules/home-manager/cli/zsh/default.nix
+++ modules/home-manager/cli/zsh/default.nix
@@ -179,7 +179,7 @@ in
           }
           {
             name = "zsh-fast-syntax-highlighting";
-            file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+            file = "share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh";
             src = zsh-fast-syntax-highlighting;
           }
           {
