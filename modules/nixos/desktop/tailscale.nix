0e8d2c43d6a1e93108d1ef7919172a68ed749f0b
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-11-16 11:45:55 +0000

    nixos/tailscale: restart tailscaled.service if NetworkManager.service is restarted

--- modules/nixos/desktop/tailscale.nix
+++ modules/nixos/desktop/tailscale.nix
@@ -5,14 +5,19 @@
     default = config.nixos.desktop.enable;
   };
 
-  config = lib.mkIf config.nixos.desktop.tailscale.enable {
-    services.tailscale = {
-      enable = true;
-      useRoutingFeatures = if config.nixos.server.tailscale.enable then "both" else "client";
-    };
-
-    # Disable wait online as it's causing trouble at rebuild
-    # See: https://github.com/NixOS/nixpkgs/issues/180175
-    systemd.services.NetworkManager-wait-online.enable = false;
-  };
+  config = lib.mkMerge [
+    (lib.mkIf config.nixos.desktop.tailscale.enable {
+      services.tailscale = {
+        enable = true;
+        useRoutingFeatures = if config.nixos.server.tailscale.enable then "both" else "client";
+      };
+    })
+    (lib.mkIf config.networking.networkmanager.enable {
+      # Disable wait online as it's causing trouble at rebuild
+      # See: https://github.com/NixOS/nixpkgs/issues/180175
+      systemd.services.NetworkManager-wait-online.enable = false;
+      # Restart tailscaled if NetworkManager service is restarted
+      systemd.services.tailscaled.partOf = [ "NetworkManager.service" ];
+    })
+  ];
 }
