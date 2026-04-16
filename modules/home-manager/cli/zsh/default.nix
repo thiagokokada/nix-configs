f42afff3ce0726a51c0d75f222d74d11f77dd51d
Thiago Kenji Okada <thiagokokada@gmail.com>
2026-04-16 14:07:39 +0100

    home-manager/zsh: port some configurations from prezto

--- modules/home-manager/cli/zsh/default.nix
+++ modules/home-manager/cli/zsh/default.nix
@@ -6,6 +6,7 @@
 }:
 
 let
+  inherit (config.programs.zsh) dotDir;
   cfg = config.home-manager.cli.zsh;
   get-ip = pkgs.writeShellApplication {
     name = "get-ip";
@@ -47,7 +48,6 @@ in
 
   config = lib.mkIf cfg.enable {
     home = {
-      # disable login banner
       file =
         let
           compileZshConfig =
@@ -64,6 +64,7 @@ in
               '';
         in
         {
+          # disable login banner
           ".hushlogin".text = "";
           ".zprofile.zwc".source = compileZshConfig ".zprofile";
           ".zshenv.zwc".source = compileZshConfig ".zshenv";
@@ -113,6 +114,26 @@ in
           enable = true;
         };
 
+        completionInit = # bash
+          ''
+            # Load and initialize the completion system ignoring insecure directories with a
+            # cache time of 20 hours, so it should almost always regenerate the first time a
+            # shell is opened each day.
+            autoload -Uz compinit
+            _comp_path="${dotDir}/.zcompdump"
+            # #q expands globs in conditional expressions
+            if [[ $_comp_path(#qNmh-20) ]]; then
+              # -C (skip function check) implies -i (skip security check).
+              compinit -C -d "$_comp_path"
+            else
+              mkdir -p "$_comp_path:h"
+              compinit -i -d "$_comp_path"
+              # Keep $_comp_path younger than cache time even if it isn't regenerated.
+              touch "$_comp_path"
+            fi
+            unset _comp_path
+          '';
+
         envExtra = lib.mkBefore (
           lib.optionalString config.home-manager.darwin.enable
             # bash
@@ -138,17 +159,31 @@ in
           (lib.mkOrder 1000
             # bash
             ''
-              # fix "no matches found" when using glob characters
-              setopt no_nomatch
-              # disable partial match
-              unsetopt completeinword
-              # disable clock
-              unset RPROMPT
+              setopt NO_NOMATCH           # Fix "no matches found" when using glob characters
+              setopt ALWAYS_TO_END        # Move cursor to the end of a completed word.
+              setopt PATH_DIRS            # Perform path search even on command names with slashes.
+              setopt AUTO_MENU            # Show completion menu on a successive tab press.
+              setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
+              setopt AUTO_PARAM_SLASH     # If completed parameter is a directory, add a trailing slash.
+              setopt EXTENDED_GLOB        # Needed for file modification glob modifiers with compinit.
+              unsetopt COMPLETE_IN_WORD   # Do not complete from both ends of a word.
+              unsetopt MENU_COMPLETE      # Do not autoselect the first completion entry.
+              unsetopt FLOW_CONTROL       # Disable start/stop characters in shell editor.
 
-              # map V in vi-mode to edit the current command line in $VISUAL
+              # Map V in vi-mode to edit the current command line in $VISUAL
               bindkey -M vicmd 'V' edit-command-line
 
+              # Pure related options
+              unset RPROMPT # disable clock
               zstyle ':prompt:pure:prompt:success' color 39 # miku color
+
+              # Defaults.
+              zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
+              zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+
+              # Use caching to make completion for commands usable.
+              zstyle ':completion::complete:*' use-cache on
+              zstyle ':completion::complete:*' cache-path "${dotDir}/.zcompdump"
             ''
           )
           (lib.mkOrder 1200 (
