e37d15f566a78e36947aa8b42c3c3043929f6bf4
Thiago Kenji Okada <thiagokokada@gmail.com>
2026-05-23 12:33:19 +0100

    ansible/chibi-miku: add touchscreen-reset.sh hack

---
+++ ansible/files/touchscreen-reset.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "${1}" = "post" ]; then
+    DEV_ID="4-0001"
+    DRIVER_PATH="/sys/bus/i2c/drivers/i2c_hid_of"
+
+    echo "$DEV_ID" > "$DRIVER_PATH/unbind"
+    echo "$DEV_ID" > "$DRIVER_PATH/bind"
+fi
