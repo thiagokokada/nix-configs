61af2448e529dfceaf49a2a9ea541665c524d84a
Thiago Kenji Okada <thiagokokada@gmail.com>
2026-09-09 13:49:56 +0100

    nixos/laptop: remove tlp, add tuned

--- hosts/nixos/mirai-nixos/default.nix
+++ hosts/nixos/mirai-nixos/default.nix
@@ -30,7 +30,6 @@
   nixos = {
     dev.platformio.enable = true;
     desktop.gnome.enable = true;
-    laptop.tlp.enable = true;
     games.steam.enable = true;
     server = {
       ssh.enable = true;
@@ -43,8 +42,6 @@
   services = {
     # For fingerprint scanner
     fprintd.enable = true;
-    # Using TLP
-    power-profiles-daemon.enable = false;
   };
 
   # https://nwildner.com/posts/2024-06-05-dell-laptop-suspend/
